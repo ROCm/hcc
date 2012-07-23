8d16cf33f7acccbf0b3a86dbe5e71f49ef3fee8b
Hee-Seok Kim <devnull@localhost>
2012-07-23 12:35:38 -0500

    Added checker/cloner for clang.

---
+++ scripts/cmake/EnsureCLANGisPresent.cmake
@@ -0,0 +1,14 @@
+macro(ensure_clang_is_present dest_dir ${name})
+
+if(EXISTS "${dest_dir}/${name}/tools/clang")
+  MESSAGE("clang is present.")
+else(EXISTS "${dest_dir}/${name}/tools/clang")
+  MESSAGE("Cloning clang at ${dest_dir}/${name}/tools/clang...")
+  MESSAGE("Enter your ID of the repository:")
+  Find_Package(Mercurial)
+  Find_Program(MERCIRUAL_EXECUTABLE hg)
+  execute_process( COMMAND ${MERCURIAL_EXECUTABLE} clone https://bitbucket.org/multicoreware/cppamp -u cppamp ${dest_dir}/${name}/tools/clang )
+endif(EXISTS "${dest_dir}/${name}/tools/clang")
+
+endmacro()
+
