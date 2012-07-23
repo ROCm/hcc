697710233e7afe90148624b45ac78f87f42f10d3
Pei-Ci Wu <devnull@localhost>
2012-07-23 16:12:17 -0500

    Need to run 'hg push' manually to push the changes

--- scripts/sync_clang.py
+++ scripts/sync_clang.py
@@ -6,20 +6,22 @@ synDir = "../../../../sync_clangsvn/clangsvn"
 print "Change directory to ../../../../sync_clangsvn/clangsvn"
 os.chdir(synDir)
 
-print "Do: hg pull"
+print "Run: hg pull"
 os.system("hg pull")
 
 print "Change directory back to clang"
 os.chdir(cwd)
 
-print "Do: hg update default"
+print "Run: hg update default"
 os.system("hg update default")
 
-print "Do: hg pull %s" % synDir
+print "Run: hg pull %s" % synDir
 os.system("hg pull " + synDir)
 
-print "Do: hg update"
+print "Run: hg update"
 os.system("hg update")
 
-print "Do: hg update cppamp"
+print "Run: hg update cppamp"
 os.system("hg update cppamp")
+
+print "(you need run 'hg push' to push changes)"
