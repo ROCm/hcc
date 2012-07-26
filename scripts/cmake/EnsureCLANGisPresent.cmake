afb90574bde9ff437ec382a6c0c5aaf6a08c49d2
Hee-Seok Kim <devnull@localhost>
2012-07-26 16:20:46 -0500

    Modified README.TXT and scripts for new distributed development policy via bitbucket.

--- scripts/cmake/EnsureCLANGisPresent.cmake
+++ scripts/cmake/EnsureCLANGisPresent.cmake
@@ -1,13 +1,12 @@
-macro(ensure_clang_is_present dest_dir name)
+macro(ensure_clang_is_present dest_dir name url)
 
 if(EXISTS "${dest_dir}/${name}/tools/clang")
   MESSAGE("clang is present.")
 else(EXISTS "${dest_dir}/${name}/tools/clang")
-  MESSAGE("Cloning clang at ${dest_dir}/${name}/tools/clang...")
-  MESSAGE("Enter your ID of the repository:")
+  MESSAGE("Cloning clang from ${url}...")
   Find_Package(Mercurial)
   Find_Program(MERCIRUAL_EXECUTABLE hg)
-  execute_process( COMMAND ${MERCURIAL_EXECUTABLE} clone https://bitbucket.org/multicoreware/cppamp -u cppamp ${dest_dir}/${name}/tools/clang )
+  execute_process( COMMAND ${MERCURIAL_EXECUTABLE} clone ${url} -u cppamp ${dest_dir}/${name}/tools/clang )
 endif(EXISTS "${dest_dir}/${name}/tools/clang")
 
 endmacro()
