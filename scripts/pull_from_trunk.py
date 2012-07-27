99791df6d1a36d564b0a420f683cc321801ea313
Hee-Seok Kim <devnull@localhost>
2012-07-27 12:00:08 -0500

    [Cleanup] Added a script to pull from the main repository.

---
+++ scripts/pull_from_trunk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+cur_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+top_dir = os.path.abspath(cur_dir + os.sep + "..")
+clang_dir = top_dir + os.sep + "compiler" + os.sep + "tools" + os.sep + "clang"
+
+print
+print "**********************************************************************"
+print
+yesno = raw_input("Do you want to pull and update cppamp-driver [y/n]? ")
+if yesno.lower() in [ 'y', 'yes' ]:
+  os.chdir(top_dir)
+  os.system("hg pull https://bitbucket.org/multicoreware/cppamp-driver")
+else:
+  print "skipped."
+
+print
+print "**********************************************************************"
+print
+yesno = raw_input("Do you want to pull and update cppamp [y/n]? ")
+if yesno.lower() in [ 'y', 'yes' ]:
+  os.chdir(clang_dir)
+  os.system("hg pull https://bitbucket.org/multicoreware/cppamp")
+  print "skipped."
+
+print
+print "Done! You have to 'hg update' for those updated repositories."
+print
+
