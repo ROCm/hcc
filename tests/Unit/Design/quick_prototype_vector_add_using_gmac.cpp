81f6cf731100b4aa4803d9fd8da9661d17a7aa23
Pei-Ci Wu <devnull@localhost>
2012-07-31 16:39:19 -0500

    [AMP-1] Added a test case.

---
+++ tests/Unit/Design/quick_prototype_vector_add_using_gmac.cpp
@@ -0,0 +1,57 @@
+// RUN: %clang_cppamp -Werror -c %s
+
+#include <stdlib.h>
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <amp.h>
+using namespace Concurrency;
+
+#define N 10
+
+void randomInit(std::vector<float>& data, int n)
+{
+    for (int i = 0; i < n; ++i)
+	data[i] = rand() / (float)RAND_MAX;
+}
+
+class functor {
+public:
+  functor(array<float>& a,
+          array<float>& b,
+          array<float>& c)
+    : a(a)
+    , b(b)
+    , c(c) { }
+
+  void operator()(index<1> idx) restrict(amp) {
+    c[idx] = a[idx] + b[idx];
+  }
+
+private:
+  array<float>& a;
+  array<float>& b;
+  array<float>& c;
+};
+
+void vectorAdd_by_array(const std::vector<float>& vecA, const std::vector<float>& vecB){
+   array<float> A(N, vecA.begin());
+   array<float> B(N, vecB.begin());
+   array<float> C(N);
+   extent<1> e(N);
+
+   parallel_for_each(e, functor(A, B, C));
+}
+
+int main(void)
+{
+    std::vector<float> vecA(N);
+    std::vector<float> vecB(N);
+    randomInit(vecA, N);
+    randomInit(vecB, N);
+
+    vectorAdd_by_array(vecA, vecB);
+
+    return 0;
+}
+
