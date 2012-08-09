ed4d25cdbf2aabc7b1b01f75f466d00788fce301
Hee-Seok Kim <devnull@localhost>
2012-08-09 12:18:35 -0500

    [Refactor] Added CMake rules to build app using cxxamp compiler.

---
+++ lib/mcwamp.cpp
@@ -0,0 +1,2 @@
+#include <amp.h>
+
---
+++ scripts/cmake/MCWAMP.cmake
@@ -0,0 +1,36 @@
+include (CMakeForceCompiler)
+
+# libc++
+set(LIBCXX_SRC_DIR "${PROJECT_SOURCE_DIR}/libc++/libcxx")
+set(LIBCXX_INC_DIR "${LIBCXX_SRC_DIR}/include")
+set(LIBCXX_LIB_DIR "${PROJECT_BINARY_DIR}/libc++/libcxx/lib")
+
+# libcxxrt
+set(LIBCXXRT_LIB_DIR "${PROJECT_BINARY_DIR}/libc++/libcxxrt/lib")
+
+# gtest
+set(GTEST_SRC_DIR "${PROJECT_SOURCE_DIR}/utils")
+set(GTEST_INC_DIR "${PROJECT_SOURCE_DIR}/utils")
+
+# MCWAMP
+seT(MCWAMP_INC_DIR "${PROJECT_SOURCE_DIR}/include")
+
+# CXXAMPFLAGS
+set(CXXAMP_FLAGS "-I${GTEST_INC_DIR} -I${LIBCXX_INC_DIR} -I${MCWAMP_INC_DIR} -stdlib=libc++ -std=c++amp -DGTEST_HAS_TR1_TUPLE=0")
+
+# STATIC ONLY FOR NOW.
+macro(add_mcwamp_library name )
+  CMAKE_FORCE_CXX_COMPILER("${PROJECT_BINARY_DIR}/compiler/bin/clang++" MCWAMPCC)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXXAMP_FLAGS}")
+
+  add_library( ${name} ${ARGN} )
+endmacro(add_mcwamp_library name )
+
+macro(add_mcwamp_executable name )
+  link_directories(${LIBCXX_LIB_DIR} ${LIBCXXRT_LIB_DIR})
+  CMAKE_FORCE_CXX_COMPILER("${PROJECT_BINARY_DIR}/compiler/bin/clang++" MCWAMPCC)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I${GTEST_INC_DIR} -I${LIBCXX_INC_DIR} -I${MCWAMP_INC_DIR} -stdlib=libc++ -std=c++amp" )
+  add_executable( ${name} ${ARGN} )
+  target_link_libraries( ${name} mcwamp cxxrt dl pthread)
+endmacro(add_mcwamp_executable name )
+
