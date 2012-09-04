7404497610e59caa2946aed611b002d36a67edbf
I-Jui Sung <ray@multicorewareinc.com>
2012-09-04 10:31:17 -0500

    [Refactor] revise cmake for switching llvm version

--- scripts/cmake/EnsureLLVMisPresent.cmake
+++ scripts/cmake/EnsureLLVMisPresent.cmake
@@ -2,8 +2,7 @@ macro(ensure_llvm_is_present dest_dir name)
 
 if(EXISTS "${dest_dir}/${name}")
 MESSAGE("LLVM seems to present. Force updating to version "${LLVM_REVISION})
-execute_process( COMMAND ${Subversion_SVN_EXECUTABLE} switch -r ${LLVM_REVISION} http://llvm.org/svn/llvm-project/llvm/trunk )
-
+execute_process( COMMAND ${Subversion_SVN_EXECUTABLE} switch -r ${LLVM_REVISION} http://llvm.org/svn/llvm-project/llvm/trunk ${dest_dir}/${name} )
 execute_process( COMMAND ${Subversion_SVN_EXECUTABLE} switch -r ${LLVM_REVISION} -q http://llvm.org/svn/llvm-project/compiler-rt/trunk ${dest_dir}/${name}/projects/compiler-rt )
 else(EXISTS "${dest_dir}/${name}")
 Find_Package(Subversion)
