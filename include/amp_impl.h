56a4f7d2d543ac95ae6dd556cce301494ad9417a
I-Jui Sung <ray@multicorewareinc.com>
2012-09-16 18:21:26 -0500

    [AMP-36 Bug] Testcase

--- include/amp_impl.h
+++ include/amp_impl.h
@@ -3,7 +3,7 @@
 // Specialization of AMP classes/templates
 
 namespace Concurrency {
-
+extern "C" int get_global_id(int n) restrict(amp);
 // 1-D Concurrency::index specialization
 template<>
 class index<1> {
@@ -11,6 +11,8 @@ class index<1> {
   explicit index(int i0) restrict(amp,cpu):index_(i0) {}
   int operator[](unsigned int c) const restrict(amp,cpu) { return index_; }
  private:
+  __attribute__((annotate("__cxxamp_opencl_index")))
+  index(void) restrict(amp):index_(get_global_id(0)) {}
   int index_;
 };
 } //namespace Concurrency
