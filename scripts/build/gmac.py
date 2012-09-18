9d9449a9bcd2cdfb995134c9107c05756ae28a17
I-Jui Sung <ray@multicorewareinc.com>
2012-09-18 17:17:53 -0500

    [Refactor] support make install in the toplevel build system, gmac, and libc++rt

--- scripts/build/gmac.py
+++ scripts/build/gmac.py
@@ -10,12 +10,12 @@ def download(url, target):
   if not os.path.exists(target):
     commands.clone(ui.ui(), url, target)
 
-def build(target, inc_dir, lib_dir):
+def build(target, inc_dir, lib_dir, prefix):
   # platform dependent
   # ./configure
   if sys.platform.find('linux') != -1: # linux
     os.chdir(target)
-    os.system("./configure --enable-opencl --with-opencl-include=%s --with-opencl-library=%s --enable-tests" % (inc_dir, lib_dir)) 
+    os.system("./configure --enable-installer --enable-opencl --with-opencl-include=%s --with-opencl-library=%s --enable-tests --prefix=%s" % (inc_dir, lib_dir, prefix)) 
     os.system("make")
   else:
     print "Error: not supported platform."
@@ -28,17 +28,17 @@ def test():
     print "Error: gmac: test failed."
     sys.exit(1)
 
-def run(url, target, inc_dir, lib_dir):
+def run(url, target, inc_dir, lib_dir, prefix):
   download(url, target)
-  build(target, inc_dir, lib_dir)
+  build(target, inc_dir, lib_dir, prefix)
   test()
 
 target = sys.argv[1]
 inc_dir = sys.argv[2]
 lib_dir = sys.argv[3]
-onoff = sys.argv[4]
-
+prefix = sys.argv[4]
+onoff = sys.argv[5]
 if onoff == 'ON':
-  run(url, target, inc_dir, lib_dir)
+  run(url, target, inc_dir, lib_dir, prefix)
 
 
