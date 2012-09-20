e81e5aa6d0f1a4a6a7e197de4f2ed4eb58711bba
xiaoyaocust <devnull@localhost>
2012-09-20 09:20:30 +0800

    [AMP-47 Bug] GPU-side deserialization code generation(body, inherit class)

---
+++ tests/Unit/Codegen/deser_def_body_compound_support_inheritclass.cpp
@@ -0,0 +1,50 @@
+// RUN: %amp_device -D__GPU__=1 %s -c -o %t.device.o
+// RUN: %gtest_amp %s %t.device.o -o %t && %t
+
+#include <stdlib.h>
+#ifndef __GPU__ //gtest requires rtti, but amp_device forbids rtti
+#include <gtest/gtest.h>
+#endif 
+class Member {
+ public:
+  // Compiler-generated constructor
+  __attribute__((annotate("deserialize"))) Member(float*, int) restrict(amp);
+  float* bzzt;
+  int zzz;
+};
+
+class base {
+ public:
+  base(void): m(NULL, 3), foo(1234) {}
+  // Compiler-generated constructor
+  __attribute__((annotate("deserialize"))) base(float *m1, int m2,
+    int foo_, float bar_) restrict(amp);
+
+  Member m;
+  int foo;
+  float bar;
+};
+
+class baz :public base {
+ public:
+  baz(void): baz_foo(1234) {}
+  // Compiler-generated constructor
+  __attribute__((annotate("deserialize"))) baz(float *m1, int m2,
+    int foo_, float bar_, int bar_foo_) restrict(amp);
+  int baz_foo;
+};
+
+int fake_use(void) restrict(amp) {
+  baz bll(NULL, 0,  1, 2.0, 1);
+  return bll.foo;
+}
+#ifndef __GPU__
+TEST(GPUCodeGen, ConstructorCompound) {
+  float local_float = 2.78f;
+  baz bll(&local_float, 2, 1, 2.0,1);
+  EXPECT_EQ(bll.foo, 1);
+  EXPECT_EQ(bll.m.bzzt, &local_float);
+  EXPECT_EQ(bll.m.zzz, 2);
+  EXPECT_EQ(bll.baz_foo, 1);
+}
+#endif
