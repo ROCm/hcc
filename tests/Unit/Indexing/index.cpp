fadfffaeeaf26bd0f69f2d0e5a3c5abba8b2de82
I-Jui Sung <ray@multicorewareinc.com>
2012-09-20 19:28:14 -0500

    [AMP-32 Bug] Additional tests

--- tests/Unit/Indexing/index.cpp
+++ tests/Unit/Indexing/index.cpp
@@ -25,4 +25,18 @@ TEST(ClassIndex, Index1D) {
   Concurrency::index<1> i(n0);
   EXPECT_EQ(n0, i[0]);
 }
+
+TEST(ClassIndex, Def) {
+  Concurrency::index<1> i(1234);
+  // Test copy constructor
+  Concurrency::index<1> j(i);
+  EXPECT_EQ(i[0], j[0]);
+  // Test prefix ++
+  ++j;
+  EXPECT_EQ(i[0]+1, j[0]);
+  // Test postfix ++
+  Concurrency::index<1> k(j++);
+  EXPECT_EQ(i[0]+1, k[0]);
+  EXPECT_EQ(i[0]+2, j[0]);
+}
 #endif
