6bed491e5c20c518c63827c24f874f20d7ab33a4
I-Jui Sung <ray@multicorewareinc.com>
2012-09-21 10:41:48 -0500

    [Refactor] Refactor ocl_accel.cpp

--- tests/Unit/Accelerator/ocl_accel.cpp
+++ tests/Unit/Accelerator/ocl_accel.cpp
@@ -23,39 +23,30 @@ TEST(Accelerator, Creation) {
     sizeof(cl_context), &ret_ctx, NULL);
   // Expect res_ctx to be the same
   EXPECT_EQ(ret_ctx, c);
-}
 
-TEST(Accelerator, View) {
-  Concurrency::accelerator def;
-
-  Concurrency::accelerator_view av1 = def.get_default_view();
-
+  cl_uint ret1;
   // to get the internal OpenCL context out of accelerator_view
-  cl_context c = av1.clamp_get_context();
-  cl_int res;
-  cl_uint ret;
+  c = def.get_default_view().clamp_get_context();
   res = clGetContextInfo(c, CL_CONTEXT_REFERENCE_COUNT,
-    sizeof(cl_uint), &ret, NULL);
+    sizeof(cl_uint), &ret1, NULL);
+  EXPECT_EQ(CL_SUCCESS, res);
+
+  cl_uint ret2;
+  // A new view constructed
+  Concurrency::accelerator_view av1 = def.get_default_view();
+  c = av1.clamp_get_context();
+  res = clGetContextInfo(c, CL_CONTEXT_REFERENCE_COUNT,
+    sizeof(cl_uint), &ret2, NULL);
   // Expect a valid OpenCL context is constructed
   EXPECT_EQ(CL_SUCCESS, res);
-  EXPECT_EQ(2, ret);
+  EXPECT_EQ(ret1+1, ret2);
 
   // Test copy constructor
   Concurrency::accelerator_view av2(av1);
-  cl_uint old_ret = ret;
+  cl_uint ret3;
   res = clGetContextInfo(c, CL_CONTEXT_REFERENCE_COUNT,
-    sizeof(cl_uint), &ret, NULL);
+    sizeof(cl_uint), &ret3, NULL);
   // Expect a valid OpenCL context is constructed
   EXPECT_EQ(CL_SUCCESS, res);
-  EXPECT_EQ(old_ret+1, ret);
-
-  // Test view creation
-  av2 = def.create_view();
-  cl_uint assign_ret = ret;
-  res = clGetContextInfo(c, CL_CONTEXT_REFERENCE_COUNT,
-    sizeof(cl_uint), &ret, NULL);
-  // Expect a valid OpenCL context is constructed
-  EXPECT_EQ(CL_SUCCESS, res);
-  // Old view should be released
-  EXPECT_EQ(old_ret, ret);
+  EXPECT_EQ(ret2+1, ret3);
 }
