e106cb25ee162f69949761f636b7e2a5a507e30a
I-Jui Sung <ray@multicorewareinc.com>
2012-09-27 17:03:55 -0500

    [AMP-58 Bug] A testcase that would fail w/o barrier

---
+++ tests/Unit/Design/lambda_tiled_local.cpp
@@ -0,0 +1,48 @@
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O3 -o %t.ll 
+// RUN: mkdir -p %t
+// RUN: %llc -march=c -o %t/kernel.cl < %t.ll
+// RUN: pushd %t 
+// RUN: objcopy -B i386:x86-64 -I binary -O elf64-x86-64 kernel.cl %t/kernel.o
+// RUN: popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+#include <amp.h>
+#include <stdlib.h>
+#include <iostream>
+int main(void){
+  const int vecSize = 1280;
+#define TILE 128
+  // Alloc & init input data
+  Concurrency::extent<1> e(vecSize);
+  Concurrency::tiled_extent<TILE> et(e);
+  Concurrency::tiled_extent<TILE> et2 = e.tile<TILE>();
+  assert(et.tile_dim0 == TILE);
+  assert(et2.tile_dim0 == TILE);
+  Concurrency::array<int, 1> a(vecSize);
+  Concurrency::array<int, 1> b(vecSize);
+  Concurrency::array<int, 1> c(vecSize);
+  int sum = 0;
+  for (Concurrency::index<1> i(0); i[0] < vecSize; i++) {
+    a[i] = 100.0f * rand() / RAND_MAX;
+    b[i] = 100.0f * rand() / RAND_MAX;
+    sum += a[i] + b[i];
+  }
+
+  Concurrency::array_view<int> ga(a);
+  Concurrency::array_view<int> gb(b);
+  Concurrency::array_view<int> gc(c);
+  Concurrency::parallel_for_each(
+    et,
+    [=](Concurrency::tiled_index<TILE> idx) restrict(amp) {
+    tile_static int shm[TILE];
+    shm[idx.local[0]] = ga[idx];
+    idx.barrier.wait();
+    gc[idx] = shm[(TILE-1)-idx.local[0]]+gb[idx];
+  });
+
+  int error = 0;
+  for(unsigned i = 0; i < vecSize; i++) {
+    error += gc[i] - (ga[i] + gb[i]);
+  }
+  std::cout << "Error = " << error << "\n";
+  return error != 0;
+}
