73a11422028fed2bb899e8aa54956da49ab7a55a
I-Jui Sung <ray@multicorewareinc.com>
2012-10-09 11:53:39 -0500

    [AMP-67 Bug] Minimized testcase from Bitonic Sort

---
+++ tests/Unit/Codegen/tworef.cpp
@@ -0,0 +1,14 @@
+// RUN: %amp_device -c -S -emit-llvm %s
+#include <amp.h>
+
+using namespace concurrency;
+
+int main()
+{
+  unsigned length = 16;
+  array<int, 1> temp(length);
+  array<int, 1> data(length);
+  extent<1> cdomain_transpose(16);
+  parallel_for_each (cdomain_transpose, [=, &data, &temp] (index<1> tidx)  restrict(amp) {});
+  return 0;
+}
