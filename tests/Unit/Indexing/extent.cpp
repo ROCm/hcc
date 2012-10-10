8e6d0d441f776c2e3537595dc766a2f1f9df1d8b
I-Jui Sung <ray@multicorewareinc.com>
2012-10-10 17:27:39 -0500

    [AMP-68 Bug] extent used in kernel always return 0

---
+++ tests/Unit/Indexing/extent.cpp
@@ -0,0 +1,31 @@
+// RUN: %amp_device -D__GPU__=1 %s -m32 -emit-llvm -c -S -O3 -o %t.ll 
+// RUN: mkdir -p %t
+// RUN: %llc -march=c -o %t/kernel.cl < %t.ll
+// RUN: pushd %t 
+// RUN: objcopy -B i386:x86-64 -I binary -O elf64-x86-64 kernel.cl %t/kernel.o
+// RUN: popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+
+#include <iostream> 
+#include <amp.h>
+#include <vector>
+using namespace concurrency; 
+int main() 
+{
+  std::vector<int> vv(10);
+  for (int i = 0; i<10; i++)
+    vv[i] = i+2;
+
+  extent<2> e(5, 2);
+  {
+    array_view<int, 2> av(5, 2, vv.data()); 
+    parallel_for_each(av.extent, [=](index<2> idx) restrict(amp) { 
+	av(idx) -= av.extent[1]; 
+    });
+    assert(av.extent == e);
+    for(unsigned int i = 0; i < av.extent[0]; i++)
+      for(unsigned int j = 0; j < av.extent[1]; j++)
+	assert(i*2+j == static_cast<char>(av(i, j)));
+  }
+  return 0;
+}
