c94ccb21ee8a2a2f5257bf341b6e6ffc9547ad5d
UncleHandsome <yunwei@multicorewareinc.com>
2013-09-06 13:06:46 -0500

    [AMP-94 Bug] Missing implementation of array<T, 1>

---
+++ tests/Conformance/4_Basic_Data_Elmnts/4_1_index/4_1_2_c/Copy/Test.01/test.cpp
@@ -0,0 +1,69 @@
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O3 -o %t.ll && mkdir -p %t
+// RUN: %llc -march=c -o %t/kernel_.cl < %t.ll
+// RUN: cat %opencl_math_dir/opencl_math.cl %t/kernel_.cl > %t/kernel.cl
+// RUN: pushd %t && objcopy -B i386:x86-64 -I binary -O elf64-x86-64 kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+
+#include "../../../Helpers/IndexHelpers.h"
+#include <amp.h>
+using namespace Concurrency;
+using std::vector;
+
+
+/* --- All index variables are declared as Index<RANK> {0, 1, 2..} for ease of verification --- */
+const int RANK = 3;
+
+/*---------------- Test on Host ------------------ */
+bool CopyConstructWithIndexOnHost()
+{
+
+    index<RANK> idx1(0, 1, 2);
+    index<RANK> idx2(idx1);   // copy construct
+    
+    return IsIndexSetToSequence<RANK>(idx2);
+}
+
+/*---------------- Test on Device ---------------- */
+/* A returns the components of the index, B returns the Rank */
+void kernelIndex(array<int, 1>& A, array<int, 1>& B) restrict(amp,cpu)
+{
+    index<RANK> index1(0, 1, 2);
+    index<RANK> index2(index1);   // copy construct
+
+    for(int i = 0; i < RANK;i++)
+    {
+        A(i) = index2[i];
+    }
+
+    B(0) = index2.rank;
+}
+
+bool CopyConstructWithIndexOnDevice()
+{
+    vector<int> resultsA(RANK), resultsB(1);
+    array<int, 1> A((extent<1>(RANK))), B(extent<1>(1));
+
+    extent<1> ex(1);
+    parallel_for_each(ex, [&](index<1> idx) restrict(amp,cpu) {
+        kernelIndex(A, B);
+    });
+
+    resultsA = A;
+    resultsB = B;
+
+    return IsIndexSetToSequence<RANK>(resultsA, resultsB[0]);
+}
+
+
+/*--------------------- Main -------------------- */
+int main() 
+{
+    int result = 1;
+
+    // Test on host
+    result &= (CopyConstructWithIndexOnHost());
+
+    // Test on device
+    result &= (CopyConstructWithIndexOnDevice());
+    return !result;
+}
---
+++ tests/Conformance/4_Basic_Data_Elmnts/4_1_index/Helpers/IndexHelpers.h
@@ -0,0 +1,70 @@
+﻿#ifndef __INDEX_HELPERS__
+#define __INDEX_HELPERS__
+
+#include <amp.h>
+#include <vector>
+
+using namespace Concurrency;
+using std::vector;
+
+/* Expects all index[i] to be set to 0 */
+template<int Rank>
+bool IsIndexSetToZero(const index<Rank> &actual)
+{
+    bool passed = true;
+    for(int i = 0; i < Rank; i++)
+        if(actual[i] != 0)
+            passed = false;
+
+    if (actual.rank != Rank)
+        passed = false;
+
+    return passed;
+}
+
+/* Used to verify index created on device and returned as field, Expects index[i] to be set to 0 */
+template<int Rank>
+bool IsIndexSetToZero(vector<int> actual, int actualRank)
+{
+    bool passed = true;
+    for(int i = 0; i < Rank; i++)
+        if(actual[i] != 0)
+            passed = false;
+
+    if (actualRank != Rank)
+        passed = false;
+
+    return passed;
+}
+
+/* Expects all index[i] to be set to start + i */
+template<int Rank>
+bool IsIndexSetToSequence(const index<Rank> &actual, int start = 0)
+{
+    bool passed = true;
+    for(int i = 0; i < Rank; i++)
+        if(actual[i] != start + i)
+            passed = false;
+
+    if (actual.rank != Rank)
+        passed = false;
+
+    return passed;
+}
+
+/* Used to verify index created on device and returned as field. Expects index[i]) to be set to start + i */
+template<int Rank>
+bool IsIndexSetToSequence(vector<int> actual, int actualRank, int start = 0)
+{
+    bool passed = true;
+    for(int i = 0; i < Rank; i++)
+        if(actual[i] != start + i)
+            passed = false;
+
+    if (actualRank != Rank)
+        passed = false;
+
+    return passed;
+}
+
+#endif /* __INDEX_HELPERS__ */
