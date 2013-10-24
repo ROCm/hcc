eb8655d4641950b6a12168a6a8f109fe5d41ba17
UncleHandsome <yunwei@multicorewareinc.com>
2013-10-25 00:44:26 +0800

    remove test case Codegen/check_opencl.kernels.cpp

--- tests/Unit/Codegen/check_opencl.kernels.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %cxxamp -c -S -emit-llvm %s -o-|c++filt|%FileCheck %s
-
-__attribute__((opencl_kernel_function)) void kernel_function(int a) {
-}
-
-// CHECK: !opencl.kernels = !{!0}
-// CHECK: !0 = metadata !{void (i32)* @kernel_function(int)}
