20e59679d2aa342b4167ae8ce2fa3a5389c62758
ccw <ccw@ccw-K43SA.(none)>
2014-01-06 11:14:22 +0800

    [AMP-111 Bug] Implement array_view::reinterpret_as() functions.

---
+++ tests/Conformance/5_Data_Cont/5_2_a_v/5_2_5_v_o/Reinterpret_As/Test.01/test.cpp
@@ -0,0 +1,46 @@
+//--------------------------------------------------------------------------------------
+// File: test.cpp
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+// file except in compliance with the License.  You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR
+// CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing permissions
+// and limitations under the License.
+//
+//--------------------------------------------------------------------------------------
+//
+/// <tags>P1</tags>
+/// <summary>Reinterpret an AV of unsigned int as int (CPU)</summary>
+
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O3 -o %t.ll && mkdir -p %t
+// RUN: %llc -march=c -o %t/kernel_.cl < %t.ll
+// RUN: cat %opencl_math_dir/opencl_math.cl %t/kernel_.cl > %t/kernel.cl
+// RUN: pushd %t && objcopy -B i386:x86-64 -I binary -O elf64-x86-64 kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+#include "../../../../array_view_test.h"
+#include "../../../../amp.compare.h"
+#include <vector>
+#include <algorithm>
+
+using namespace Concurrency;
+using namespace Concurrency::Test;
+
+int main()
+{
+    std::vector<unsigned int> v(10);
+    Fill(v);
+    
+    array_view<unsigned int, 1> av_uint(static_cast<int>(v.size()), v);
+    array_view<int, 1> av_int = av_uint.reinterpret_as<int>();
+    
+    return Verify<int>(reinterpret_cast<int *>(av_uint.data()), av_int.data(), v.size()) ? 0 : 1;
+}
+
