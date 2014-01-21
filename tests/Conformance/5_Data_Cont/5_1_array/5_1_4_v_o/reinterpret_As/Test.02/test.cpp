bb5f4f01c61bc4a79580b7dbf92615d6c2317c5c
CC_Wang <ccw@ccw-K43SA.(none)>
2014-01-21 14:34:27 +0800

    [AMP-130 Bug] Support array_view<const T,N>::array_view(const Concurrency::extent<N>&, const T*).

---
+++ tests/Conformance/5_Data_Cont/5_1_array/5_1_4_v_o/reinterpret_As/Test.02/test.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft
+// All rights reserved
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+/// <tags>P1</tags>
+/// <summary>Reinterpret an Array of float as const int (CPU)</summary>
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O3 -o %t.ll && mkdir -p %t
+// RUN: %llc -march=c -o %t/kernel_.cl < %t.ll
+// RUN: cat %opencl_math_dir/opencl_math.cl %t/kernel_.cl > %t/kernel.cl
+// RUN: pushd %t && objcopy -B i386:x86-64 -I binary -O elf64-x86-64 kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+#include <amp.h>
+#include "../../../../amp.compare.h"
+#include "../../../../data.h"
+using namespace Concurrency;
+using namespace Concurrency::Test;
+
+int main()
+{
+    std::vector<float> v(10);
+    Fill(v);
+    
+    array<float, 1> arr_float(static_cast<int>(v.size()), v.begin());
+    array_view<const float, 1> av_float(arr_float); // Created for verification
+    array_view<const int, 1> av_int = arr_float.reinterpret_as<const int>();
+    
+    return Verify<const int>(reinterpret_cast<const int *>(av_float.data()), av_int.data(), v.size()) ? 0 : 1;
+}
+
