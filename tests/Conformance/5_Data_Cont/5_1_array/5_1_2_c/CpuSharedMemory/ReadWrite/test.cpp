5ea58b290e09a65e2d34190ccb42bdae391be64e
I-Jui (Ray) Sung <ray@multicorewareinc.com>
2014-02-10 15:24:30 -0600

    [AMP-119 Bug] Revised the testcase; the last stmt negated the result

--- tests/Conformance/5_Data_Cont/5_1_array/5_1_2_c/CpuSharedMemory/ReadWrite/test.cpp
+++ tests/Conformance/5_Data_Cont/5_1_array/5_1_2_c/CpuSharedMemory/ReadWrite/test.cpp
@@ -6,8 +6,7 @@
 
 /// <summary>Using array with CPU access type read/write on accelerator supporting zero-copy</summary>
 // RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
-// RUN: %llc -march=c -o %t/kernel_.cl < %t.ll
-// RUN: cat %opencl_math_dir/opencl_math.cl %t/kernel_.cl > %t/kernel.cl
+// RUN: %clamp-device %t.ll %t/kernel.cl
 // RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
 // RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
 #include "../Common.h"
@@ -48,6 +47,5 @@ int main()
 		Increment<DATA_TYPE, RANK>(arr, 1);
 	}
 	
-	return (ReadAndVerify<DATA_TYPE, RANK>(arr, 300));
+	return (!ReadAndVerify<DATA_TYPE, RANK>(arr, 300));
 }
-
