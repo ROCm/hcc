49acd34963e440067f491a99af77ad1c13db1044
Jack Chung <jack@multicorewareinc.com>
2014-03-11 09:28:35 -0500

    [AMP-165 Bug] incorrect C++ mangling in kernels
    Kernels in these cases wouldn't be mangled 100% according to Itanium C++ ABI. The root cause is because currently clang would only emit GPU-related codes in GPU build path, thus causing some enclosing scopes (which are CPU codes) not traversed entirely, and make the index of unnamed scopes incorrect.
    
    A possible fix could bring impacts to the following aspects:
    
    1) clang : code generation logic has be altered so in GPU path (-fcuda-is-device switch on) ALL symbols would be emitted.  This can make sure all unnamed scopes get traversed.
    
    2) opt : another pass to opt to strip out all CPU codes generated and only keep kernels for GPU.
    
    3) C++AMP lib: trampoline name escaping algorithm in parallel_for_each.h would have to be changed.
    
    4) test cases : certain unit test cases which would expect only GPU codes being emitted after GPU path might have to be changed.

---
+++ tests/Conformance/4_Basic_Data_Elmnts/4_2_extent/4_2_3_m/size/Test.01/test.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft
+// All rights reserved
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+/// <tags>P1</tags>
+/// <summary>Verify size() member function for positive size.</summary>
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
+// RUN: %clamp-device %t.ll %t/kernel.cl
+// RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+#include "./../size.h"
+
+template<typename _type>
+bool test_size() restrict(amp,cpu)
+{
+    return test_positive_size<_type>();
+}
+
+// MAIN function is located in 4_Basic_Data_Elmnts/extentbase.h
+
---
+++ tests/Conformance/4_Basic_Data_Elmnts/4_2_extent/extentbase.h
@@ -0,0 +1,106 @@
+// Copyright (c) Microsoft
+// All rights reserved
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
+// RUN: %clamp-device %t.ll %t/kernel.cl
+// RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+#include <amp.h>
+#include <vector>
+
+using std::vector;
+using namespace Concurrency;
+
+#define INVOKE_TEST_FUNC_ON_CPU_AND_GPU(_func, ...) [&]() { \
+	/* Invoke on cpu */ \
+	int cpu_result = _func(__VA_ARGS__); \
+	/* Invoke on gpu */ \
+	int gpu_result; \
+	concurrency::array_view<int, 1> gpu_resultv(1, &gpu_result); \
+	gpu_resultv.discard_data(); \
+	concurrency::parallel_for_each(gpu_resultv.get_extent() \
+		, [=](concurrency::index<1> idx) restrict(amp) { \
+		gpu_resultv[idx] = _func(__VA_ARGS__); \
+	}); \
+	gpu_resultv.synchronize(); \
+	return cpu_result & gpu_result; \
+}()
+
+/// Common Section
+template<typename _type>
+bool test_feature() restrict(amp,cpu);
+
+template <typename T>
+bool test() restrict(amp,cpu)
+{
+    return test_feature<T>();
+}
+
+/*
+int blah()
+{
+	int result = 1;
+	result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+		return test<extent<1>>();
+	});																
+	result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+		return test<extent<4>>();
+	});																
+	result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+		return test<extent<10>>();
+	});																
+
+	{
+		result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+			return test<extent<1>>();
+		});																
+		result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+			return test<extent<4>>();
+		});																
+		result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+			return test<extent<10>>();
+		});																
+
+		{
+			result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+				return test<extent<1>>();
+			});																
+			result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+				return test<extent<4>>();
+			});																
+			result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+				return test<extent<10>>();
+			});																
+
+		}
+
+	}
+
+	return result;
+}
+*/
+
+int main() 
+{
+	//accelerator_view av = require_device().get_default_view();
+	int result = 1;
+	result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+		return test<extent<1>>();
+	});
+
+	result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+		return test<extent<4>>();
+	});
+
+	result &= INVOKE_TEST_FUNC_ON_CPU_AND_GPU([]() restrict(amp,cpu){
+		return test<extent<10>>();
+	});																
+
+	//result &= blah();
+
+	return !result;
+}
