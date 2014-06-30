bd9c03761a0849bd546f96c7580b6d99abdde30f
Jack Chung <jack@multicorewareinc.com>
2014-06-29 22:16:40 -0500

    [AMP-272 Fix] add LLVM GlobalOpt patch
    GlobaOpt pass in LLVM fails to copy attributes for optimized memory access, which will let tile_static variables lose section attributes.  In this commit we patch LLVM so GlobalOpt will preserve all attributes.

--- scripts/cmake/SetupSPIRify.cmake
+++ scripts/cmake/SetupSPIRify.cmake
@@ -7,5 +7,7 @@ MESSAGE("Setting up SPIRify.")
 execute_process( COMMAND ln -s ${PROJECT_SOURCE_DIR}/${name} ${dest_dir}/${name} )
 execute_process( COMMAND sh ${PROJECT_SOURCE_DIR}/${name}/SPIRify.patch
                  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+execute_process( COMMAND sh ${PROJECT_SOURCE_DIR}/${name}/GlobalOpt.patch
+                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 endif()
 endmacro()
