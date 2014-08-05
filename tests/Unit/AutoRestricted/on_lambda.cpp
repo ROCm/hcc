4da4142078ac8d0691e21131c5ac0a2e427a379d
hui <hui@multicorewareinc.com>
2014-08-05 17:28:37 +0800

    [AMP-431 Bug] Can't parse restrict(auto). Need add support to this before any sema analysis

---
+++ tests/Unit/AutoRestricted/on_lambda.cpp
@@ -0,0 +1,40 @@
+// RUN: %amp_device -D__GPU__ %s -m32 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
+// RUN: %clamp-device %t.ll %t/kernel.cl
+// RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp %link %t/kernel.o %s -o %t.out && %t.out
+// RUN: %llvm-dis %t/kernel.cl.promote.bc -o - | %FileCheck %s
+#include <amp.h>
+#include <stdlib.h>
+#include <iostream>
+int main(void){
+  const int vecSize = 100;
+
+  // Alloc & init input data
+  Concurrency::extent<1> e(vecSize);
+  Concurrency::array<int, 1> a(vecSize);
+  Concurrency::array<int, 1> b(vecSize);
+  Concurrency::array<int, 1> c(vecSize);
+  int sum = 0;
+  for (Concurrency::index<1> i(0); i[0] < vecSize; i++) {
+    a[i] = 100.0f * rand() / RAND_MAX;
+    b[i] = 100.0f * rand() / RAND_MAX;
+    sum += a[i] + b[i];
+  }
+
+  Concurrency::array_view<int> ga(a);
+  Concurrency::array_view<int> gb(b);
+  Concurrency::array_view<int> gc(c);
+  Concurrency::parallel_for_each(
+    e,
+    [=](Concurrency::index<1> idx) restrict(amp,auto) {
+    gc[idx] = ga[idx]+gb[idx];
+  });
+
+  int error = 0;
+  for(unsigned i = 0; i < vecSize; i++) {
+    error += gc[i] - (ga[i] + gb[i]);
+  }
+  return (error != 0);
+}
+// SPIR code generation test
+// CHECK: metadata !{metadata !"kernel_arg_addr_space", i32 0, 
