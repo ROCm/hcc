f5ed962eb7e1eba0a9076daebfe419c3c374eca1
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-09-09 02:00:00 -0500

    [AMP-386] Implement Xmalloc's new & add tests

--- tests/Unit/NewDelete/fibonacci.cpp
+++ tests/Unit/NewDelete/fibonacci.cpp
@@ -1,3 +1,4 @@
+// XFAIL: Linux
 // RUN: %amp_device -D__GPU__ -Xclang -fhsa-ext %s -m64 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
 // RUN: %clamp-device %t.ll %t/kernel.cl
 // RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
@@ -7,8 +8,9 @@
 #include <atomic>
 #include <thread>
 #include <amp.h>
+#include <ctime>
 
-#define TEST_DEBUG 1
+#define DEBUG 1
 
 void put_ptr_a(void* addr) restrict(amp);
 void put_ptr_b(void* addr) restrict(amp);
@@ -18,19 +20,25 @@ void put_ptr_c(void* addr) restrict(amp);
 int main ()
 {
   // define inputs and output
-  const int vecSize = 5;
+  const int vecSize = 16;
+  const int tileSize = 4;
+  const int tileCount = vecSize / tileSize;
   const int cpuSleepMsec = 25;
 
+  #define MAX_VEC_SIZE 10240
+  #define MAX_TILE_SIZE 256
+  #define MAX_TILE_COUNT MAX_VEC_SIZE
+
   // specify syscall number
-  std::atomic_int table_a[vecSize];
+  std::atomic_int table_a[MAX_TILE_COUNT];
   auto ptr_a = &table_a[0];
 
   // syscall parameter
-  std::atomic_long table_b[vecSize];
+  std::atomic_long table_b[MAX_TILE_COUNT];
   auto ptr_b = &table_b[0];
 
   // test result
-  std::atomic_long table_c[vecSize];
+  std::atomic_long table_c[MAX_VEC_SIZE];
   auto ptr_c = &table_c[0];
 
   // CPU syscall service thread control
@@ -38,73 +46,125 @@ int main ()
   auto ptr_done = &done;
 
   // initialize test data
-  for (int i = 0; i < vecSize; ++i) {
+  for (int i = 0; i < MAX_TILE_COUNT; ++i) {
     table_a[i].store(0);
     table_b[i].store(0);
+  }
+
+  for (int i = 0; i < MAX_VEC_SIZE; ++i) {
     table_c[i].store(0);
   }
 
+  int syscall_count = 0;
+
   // fire CPU thread
-  std::thread cpu_thread([=]() {
+  std::thread cpu_thread([=, &syscall_count]() {
     std::cout << "Enter CPU syscall service thread..." << std::endl;
     std::chrono::milliseconds dura( cpuSleepMsec );
     int syscall;
     while (!*ptr_done) {
-      for (int i = 0; i < vecSize; ++i) {
+      for (int i = 0; i < MAX_TILE_COUNT; ++i) {
         syscall = (ptr_a + i)->load(std::memory_order_acquire);
 
         if (syscall) {
+          syscall_count++;
           // load parameter
           long param = (ptr_b + i)->load(std::memory_order_acquire);
-          
+
           // do actual stuff
           long result;
           switch (syscall) {
             case 1: // malloc
-              result = (long)memalign(0x1000, param);
-              std::cout << std::dec << "tid: " << i << ", malloc(" << param << "), "
+              result = (long)malloc(param);
+#if DEBUG
+              std::cout << std::dec << "malloc(" << param << "), "
                 << "ret: " << "0x" << std::setfill('0') << std::setw(2) << std::hex << result << "\n";
+#endif
             break;
             case 2: // free
-              std::cout << std::dec << "tid: " << i << ", free(" << std::hex << param << ")\n";
+#if DEBUG
+              std::cout << std::dec << ", free(" << std::hex << param << ")\n";
+#endif
               free((void*)param);
               result = 0;
             break;
           }
 
           // store result
-          (ptr_b + i)->store(result, std::memory_order_release); 
+          (ptr_b + i)->store(result, std::memory_order_release);
 
           // reset flag
           (ptr_a + i)->store(0, std::memory_order_release);
         }
-      }
 
-      std::this_thread::sleep_for( dura );
+      }
+        std::this_thread::sleep_for( dura );
     }
-    std::cout << "Leave CPU syscall service thread." << std::endl;
+      std::cout << "Leave CPU syscall service thread." << std::endl;
   });
 
   // launch kernel
   unsigned long int sumCPP[vecSize];
   Concurrency::array_view<unsigned long int, 1> sum(vecSize, sumCPP);
+
+  clock_t m_start = clock();
   parallel_for_each(
-    sum.get_extent(),
+    Concurrency::extent<1>(vecSize),
     [=](Concurrency::index<1> idx) restrict(amp) {
 
     put_ptr_a(ptr_a);
     put_ptr_b(ptr_b);
     put_ptr_c(ptr_c);
 
-    unsigned int *p = new unsigned int[2];
-    sum[idx] = (unsigned long int)p;
-    delete p;
+    int tid = idx[0];
+    int *fib = new int[(tid < 2) ? 2 : tid + 1];
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= tid; ++i)
+    {
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    sum[idx[0]] = fib[tid];
+    delete[] fib;
   });
+  clock_t m_stop = clock();
 
   // stop CPU thread
   done.store(true);
   cpu_thread.join();
 
-  int error = 0;
-  return (error != 0);
+#if DEBUG
+  for (int i = 0; i < vecSize; i++)
+  {
+    printf("Fib[n] is %lu\n", sum[i]);
+  }
+#endif
+
+  // Verify
+  int *fibh = new int[vecSize + 1];
+
+  fibh[0] = 0;
+  fibh[1] = 1;
+
+  for (int i = 2; i < vecSize; i++)
+  {
+    fibh[i] = fibh[i-1] + fibh[i-2];
+  }
+
+  for (int i = 0; i < vecSize; i++)
+  {
+    if (fibh[i] != sum[i]) {
+      std::cout << "Verify failed!\n";
+      return 1;
+    }
+  }
+
+  delete[] fibh;
+
+  std::cout << "Verify success!\n";
+  return 0;
 }
+
--- tests/Unit/NewDelete/new_obj_array.cpp
+++ tests/Unit/NewDelete/new_obj_array.cpp
@@ -8,9 +8,10 @@
 #include <atomic>
 #include <thread>
 #include <amp.h>
+#include <ctime>
 #include "point.h"
 
-#define TEST_DEBUG 1
+#define DEBUG 1
 
 void put_ptr_a(void* addr) restrict(amp);
 void put_ptr_b(void* addr) restrict(amp);
@@ -20,19 +21,25 @@ void put_ptr_c(void* addr) restrict(amp);
 int main ()
 {
   // define inputs and output
-  const int vecSize = 5;
+  const int vecSize = 16;
+  const int tileSize = 4;
+  const int tileCount = vecSize / tileSize;
   const int cpuSleepMsec = 25;
 
+  #define MAX_VEC_SIZE 10240
+  #define MAX_TILE_SIZE 256
+  #define MAX_TILE_COUNT MAX_VEC_SIZE
+
   // specify syscall number
-  std::atomic_int table_a[vecSize];
+  std::atomic_int table_a[MAX_TILE_COUNT];
   auto ptr_a = &table_a[0];
 
   // syscall parameter
-  std::atomic_long table_b[vecSize];
+  std::atomic_long table_b[MAX_TILE_COUNT];
   auto ptr_b = &table_b[0];
 
   // test result
-  std::atomic_long table_c[vecSize];
+  std::atomic_long table_c[MAX_VEC_SIZE];
   auto ptr_c = &table_c[0];
 
   // CPU syscall service thread control
@@ -40,76 +47,91 @@ int main ()
   auto ptr_done = &done;
 
   // initialize test data
-  for (int i = 0; i < vecSize; ++i) {
+  for (int i = 0; i < MAX_TILE_COUNT; ++i) {
     table_a[i].store(0);
     table_b[i].store(0);
+  }
+
+  for (int i = 0; i < MAX_VEC_SIZE; ++i) {
     table_c[i].store(0);
   }
 
+  int syscall_count = 0;
+
   // fire CPU thread
-  std::thread cpu_thread([=]() {
+  std::thread cpu_thread([=, &syscall_count]() {
     std::cout << "Enter CPU syscall service thread..." << std::endl;
     std::chrono::milliseconds dura( cpuSleepMsec );
     int syscall;
     while (!*ptr_done) {
-      for (int i = 0; i < vecSize; ++i) {
+      for (int i = 0; i < MAX_TILE_COUNT; ++i) {
         syscall = (ptr_a + i)->load(std::memory_order_acquire);
 
         if (syscall) {
+          syscall_count++;
           // load parameter
           long param = (ptr_b + i)->load(std::memory_order_acquire);
-          
+
           // do actual stuff
           long result;
           switch (syscall) {
             case 1: // malloc
-              result = (long)memalign(0x1000, param);
-              std::cout << std::dec << "tid: " << i << ", malloc(" << param << "), "
+              result = (long)malloc(param);
+#if DEBUG
+              std::cout << std::dec << "malloc(" << param << "), "
                 << "ret: " << "0x" << std::setfill('0') << std::setw(2) << std::hex << result << "\n";
+#endif
             break;
             case 2: // free
-              std::cout << std::dec << "tid: " << i << ", free(" << std::hex << param << ")\n";
+#if DEBUG
+              std::cout << std::dec << ", free(" << std::hex << param << ")\n";
+#endif
               free((void*)param);
               result = 0;
             break;
           }
 
           // store result
-          (ptr_b + i)->store(result, std::memory_order_release); 
+          (ptr_b + i)->store(result, std::memory_order_release);
 
           // reset flag
           (ptr_a + i)->store(0, std::memory_order_release);
         }
-      }
 
-      std::this_thread::sleep_for( dura );
+      }
+        std::this_thread::sleep_for( dura );
     }
-    std::cout << "Leave CPU syscall service thread." << std::endl;
+      std::cout << "Leave CPU syscall service thread." << std::endl;
   });
 
   // launch kernel
   unsigned long int sumCPP[vecSize];
   Concurrency::array_view<unsigned long int, 1> sum(vecSize, sumCPP);
+
+  clock_t m_start = clock();
   parallel_for_each(
-    sum.get_extent(),
+    Concurrency::extent<1>(vecSize),
     [=](Concurrency::index<1> idx) restrict(amp) {
 
     put_ptr_a(ptr_a);
     put_ptr_b(ptr_b);
     put_ptr_c(ptr_c);
 
-    sum[idx] = (unsigned long int)new Point();
+    sum[idx[0]] = (unsigned long int)new Point[2]();
   });
+  clock_t m_stop = clock();
 
   // stop CPU thread
   done.store(true);
   cpu_thread.join();
 
-#if TEST_DEBUG
+#if DEBUG
   for (int i = 0; i < vecSize; i++)
   {
     Point *p = (Point *)sum[i];
-    printf("Value of addr %p is %d & %d\n", (void*)p, p->get_x(), p->get_y());
+    printf("Value of addr %p is %d & %d, addr %p is %d & %d\n",
+      (void*)p, p->get_x(), p->get_y(),
+      (void*)(p + 1), (p + 1)->get_x(), (p + 1)->get_y());
   }
 #endif
 
@@ -118,13 +140,20 @@ int main ()
   for(int i = 0; i < vecSize; i++) {
     Point *p = (Point*)sum[i];
     Point pt;
-    error += (abs(p->get_x() - pt.get_x()) + abs(p->get_y() - pt.get_y()));
+    error += (abs(p->get_x() - pt.get_x()) + abs(p->get_y() - pt.get_y())
+               + abs((p + 1)->get_x() - pt.get_x()) + abs((p + 1)->get_y() - pt.get_y()));
   }
   if (error == 0) {
     std::cout << "Verify success!\n";
   } else {
     std::cout << "Verify failed!\n";
   }
-
+  clock_t t1 = clock();
+  clock_t t2 = clock();
+  clock_t m_overhead = t2 - t1;
+  double elapsed = ((double)(m_stop - m_start - m_overhead)) / CLOCKS_PER_SEC;
+  std::cout << "Execution time of amp restrict lambda is " << std::dec << elapsed << " s.\n";
+  std::cout << "System call is executed " << std::dec << syscall_count << " times\n";
   return (error != 0);
 }
+
