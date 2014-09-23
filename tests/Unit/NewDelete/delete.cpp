efa5e810fe4faa7eb857ffb663d27893720a76a5
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-09-23 06:15:45 -0500

    [AMP-386] Clean code

--- tests/Unit/NewDelete/delete.cpp
+++ tests/Unit/NewDelete/delete.cpp
@@ -6,10 +6,9 @@
 #include <iostream>
 #include <iomanip>
 #include <amp.h>
-#include <ctime>
 #include <hsa_new.h>
 
-#define DEBUG 0
+#define DEBUG 1
 
 // An HSA version of C++AMP program
 int main ()
@@ -31,7 +30,6 @@ int main ()
   unsigned long int sumCPP[vecSize];
   Concurrency::array_view<unsigned long int, 1> sum(vecSize, sumCPP);
 
-  clock_t m_start = clock();
   parallel_for_each(
     Concurrency::extent<1>(vecSize).tile<tileSize>(),
     [=](Concurrency::tiled_index<tileSize> tidx) restrict(amp) {
@@ -44,41 +42,28 @@ int main ()
     put_ptr_y(ptr_y);
     put_ptr_z(ptr_z);
 
-    /*sum[tidx.global[0]] = (unsigned long int)*/ 
-    unsigned int *p = new unsigned int[tidx.local[0] + 1]; //(tidx.local[0]);
+    unsigned int *p = new unsigned int[tidx.local[0] + 1];
     *p = 5566;
     sum[tidx.global[0]] = (unsigned long int)p;
     delete p;
   });
-  clock_t m_stop = clock();
 
 #if DEBUG
   for (int i = 0; i < vecSize; i++)
   {
     unsigned int *p = (unsigned int*)sum[i];
-    //printf("Value of addr %p is %u\n", (void*)p, *p);
     printf("Value of addr %p\n", (void*)p);
   }
 #endif
 
   // Verify
   int error = 0;
+  if (newInit.get_Xfree_count() != newInit.get_Xmalloc_count() + newInit.get_malloc_count()) 
+    error = 1;
   if (error == 0) {
     std::cout << "Verify success!\n";
   } else {
     std::cout << "Verify failed!\n";
   }
-  clock_t t1 = clock();
-  clock_t t2 = clock();
-  clock_t m_overhead = t2 - t1;
-  double elapsed = ((double)(m_stop - m_start - m_overhead)) / CLOCKS_PER_SEC;
-  std::cout << "Execution time of amp restrict lambda is " << std::dec << elapsed << " s.\n";
-  int Xmalloc_count = newInit.get_Xmalloc_count();
-  int malloc_count = newInit.get_malloc_count();
-  int Xfree_count = newInit.get_Xfree_count();
-  if (Xfree_count != Xmalloc_count + malloc_count) std::cout << "Verify error!\n";
-  std::cout << "Xfree_count: " << Xfree_count << "\n";
-  std::cout << "malloc_count: " << malloc_count << "\n";
-  std::cout << "Xmalloc_count: " << Xmalloc_count << "\n";
   return (error != 0);
 }
