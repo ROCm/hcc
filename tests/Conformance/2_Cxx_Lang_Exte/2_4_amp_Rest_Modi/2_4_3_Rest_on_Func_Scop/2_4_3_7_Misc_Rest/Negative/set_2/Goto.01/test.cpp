32472bce2f2bb356ae137a1839442f0196f67dde
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-09-25 05:15:32 -0500

    [AMP-404] Statements - goto/labels

---
+++ tests/Conformance/2_Cxx_Lang_Exte/2_4_amp_Rest_Modi/2_4_3_Rest_on_Func_Scop/2_4_3_7_Misc_Rest/Negative/set_2/Goto.01/test.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft
+// All rights reserved
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+/// <tags>P0</tags>
+/// <summary>test illegal usage of goto</summary>
+//#Expects: Error: error C3593
+
+// XFAIL: Linux
+// RUN: %amp_device -D__GPU__ -Xclang -fhsa-ext %s -m64 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
+// RUN: %clamp-device %t.ll %t/kernel.cl
+// RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp -Xclang -fhsa-ext %link %t/kernel.o %s -o %t.out && %t.out
+
+#include <amp.h>
+
+void foo() restrict(amp, cpu)
+{
+    goto L;
+L:
+    ;
+}
+
+int main(int argc, char **argv) 
+{ 
+    foo();
+    return 0;
+}
+
