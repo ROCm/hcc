d8c9ba70e1b628f59db96bccc8600a9b3d5b0e1a
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-09-26 06:12:25 -0500

    [AMP-404] Struct/class/union member - bitfields

---
+++ tests/Unit/CXXLangExt/struct_class_union_bitfields.cpp
@@ -0,0 +1,59 @@
+// XFAIL: Linux
+
+// RUN: %amp_device -D__GPU__ -Xclang -fhsa-ext %s -m64 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
+// RUN: %clamp-device %t.ll %t/kernel.cl
+// RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp -Xclang -fhsa-ext %link %t/kernel.o %s -o %t.out && %t.out
+
+#include <amp.h>
+
+struct S {
+  unsigned int bit : 3;
+};
+
+class C {
+public:
+  unsigned int bit : 3;
+};
+
+union U {
+  unsigned int bit : 3;
+};
+
+// An HSA version of C++AMP program
+int main ()
+{
+
+  const int vecSize = 16;
+
+  int ans[vecSize];
+  int *p_ans = &ans[0];
+
+  parallel_for_each(
+    Concurrency::extent<1>(vecSize),
+    [=](Concurrency::index<1> idx) restrict(amp) {
+
+    S s;
+    s.bit = 7;
+    ++s.bit;
+    C c;
+    c.bit = 7;
+    ++c.bit;
+    U u;
+    u.bit = 7;
+    ++u.bit;
+    p_ans[idx[0]] = (int)s.bit + (int)c.bit + (int)u.bit;
+  });
+
+  // Verify
+  int error = 0;
+  for(int i = 0; i < vecSize; i++) {
+    error += abs(ans[i]);
+  }
+  if (error == 0) {
+    std::cout << "Verify success!\n";
+  } else {
+    std::cout << "Verify failed!\n";
+  }
+  return (error != 0);
+}
