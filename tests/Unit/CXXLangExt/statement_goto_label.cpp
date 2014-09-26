6c6a276c18101a65600696577b9bbb0b25ee1eeb
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-09-26 09:04:15 -0500

    [AMP-404] Statements - goto/labels

--- tests/Unit/CXXLangExt/statement_goto_label.cpp
+++ tests/Unit/CXXLangExt/statement_goto_label.cpp
@@ -15,16 +15,40 @@
 
 #include <amp.h>
 
-void foo() restrict(amp, cpu)
+void foo() restrict(amp)
 {
     goto L;
 L:
     ;
 }
 
-int main(int argc, char **argv) 
-{ 
+// An HSA version of C++AMP program
+int main ()
+{
+
+  const int vecSize = 16;
+
+  int ans[vecSize];
+  int *p_ans = &ans[0];
+
+  parallel_for_each(
+    Concurrency::extent<1>(vecSize),
+    [=](Concurrency::index<1> idx) restrict(amp) {
+
     foo();
-    return 0;
+    p_ans[idx[0]] = idx[0];
+  });
+
+  // Verify
+  int error = 0;
+  for(int i = 0; i < vecSize; i++) {
+    error += abs(ans[i] - i);
+  }
+  if (error == 0) {
+    std::cout << "Verify success!\n";
+  } else {
+    std::cout << "Verify failed!\n";
+  }
+  return (error != 0);
 }
 
