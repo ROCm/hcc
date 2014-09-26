a58d8e15161e0e235741f25c8f2db6e9d009b365
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-09-25 23:58:44 -0500

    [AMP-404] Local/Param/Ret - half float

---
+++ tests/Unit/CXXLangExt/local_param_ret_half-float.cpp
@@ -0,0 +1,43 @@
+// XFAIL: *
+
+// RUN: %amp_device -DTYPE="half float" -D__GPU__ -Xclang -fhsa-ext %s -m64 -emit-llvm -c -S -O2 -o %t.ll && mkdir -p %t
+// RUN: %clamp-device %t.ll %t/kernel.cl
+// RUN: pushd %t && %embed_kernel kernel.cl %t/kernel.o && popd
+// RUN: %cxxamp -DTYPE="half float" -Xclang -fhsa-ext %link %t/kernel.o %s -o %t.out && %t.out
+
+#include <amp.h>
+
+TYPE func(TYPE arg) restrict(amp)
+{
+  TYPE local = arg;
+  return local;
+}
+
+// An HSA version of C++AMP program
+int main ()
+{
+
+  const int vecSize = 16;
+
+  int ans[vecSize];
+  int *p_ans = &ans[0];
+
+  parallel_for_each(
+    Concurrency::extent<1>(vecSize),
+    [=](Concurrency::index<1> idx) restrict(amp) {
+
+    p_ans[idx[0]] = func((TYPE)idx[0]);
+  });
+
+  // Verify
+  int error = 0;
+  for(int i = 0; i < vecSize; i++) {
+    error += abs((TYPE)ans[i] - (TYPE)i);
+  }
+  if (error == 0) {
+    std::cout << "Verify success!\n";
+  } else {
+    std::cout << "Verify failed!\n";
+  }
+  return (error != 0);
+}
