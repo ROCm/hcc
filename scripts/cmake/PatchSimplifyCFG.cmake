44b23aa331523e0a3b144e36461354d70125336d
Yi-Hong Lyu <yi-hong@multicorewareinc.com>
2014-10-03 13:19:39 -0500

    [AMP-439] cmake well

--- scripts/cmake/PatchSimplifyCFG.cmake
+++
@@ -1,7 +0,0 @@
-macro(patch_SimplifyCFG name)
-
-MESSAGE("Patch SimplifyCFG to aware NoDuplicate attribute.")
-execute_process( COMMAND sh ${PROJECT_SOURCE_DIR}/${name}/SimplifyCFG.diff
-                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-
-endmacro()
