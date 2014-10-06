9e71942d7d054295866af4d704e934af5831746d
UncleHandsome <yunwei@multicorewareinc.com>
2014-10-06 10:36:33 +0800

    Update amp_impl

--- include/amp_impl.h
+++ include/amp_impl.h
@@ -589,7 +589,7 @@ array<T, N>::array(const array_view<const T, N>& src, accelerator_view av,
 template <typename T, int N>
 void array_view<T, N>::synchronize() const {
     if(cache.get())
-        getAllocator().synchronize(cache.get_device());
+        cache.synchronize();
 }
 
 template <typename T, int N>
@@ -602,22 +602,18 @@ completion_future array_view<T, N>::synchronize_async() const {
 template <typename T, int N>
 array_view<T, N>::array_view(const Concurrency::extent<N>& ext,
                              value_type* src) restrict(amp,cpu)
-    : extent(ext),
-      cache(CLAllocator<T>().allocate(ext.size(), src), CLDeleter<T>()),
-      offset(0), extent_base(ext) {}
+    : extent(ext), cache(ext.size(), src), offset(0), extent_base(ext) {}
 
 template <typename T, int N>
 array_view<T, N>::array_view(const Concurrency::extent<N>& ext) restrict(amp,cpu)
-    : extent(ext),
-    cache(CLAllocator<T>().allocate(ext.size()), CLDeleter<T>()),
-    offset(0), extent_base(ext) {}
+    : extent(ext), cache(ext.size()), offset(0), extent_base(ext) {}
 
 template <typename T, int N>
 void array_view<T, N>::refresh() const {
     assert(cache.get());
     assert(extent == extent_base && "Only support non-sectioned view");
     assert(offset == 0 && "Only support non-sectioned view");
-    getAllocator().refresh(cache.get_device());
+    cache.refresh();
 }
 
 #else // GPU implementations
@@ -636,7 +632,7 @@ array_view<T,N>::array_view(const Concurrency::extent<N>& ext,
 template <typename T, int N>
 void array_view<const T, N>::synchronize() const {
   if(cache.get())
-      getAllocator().synchronize(cache.get_device());
+      cache.synchronize();
 }
 
 template <typename T, int N>
@@ -649,8 +645,7 @@ completion_future array_view<const T, N>::synchronize_async() const {
 template <typename T, int N>
 array_view<const T, N>::array_view(const Concurrency::extent<N>& ext,
                              value_type* src) restrict(amp,cpu)
-    : extent(ext),
-      cache(CLAllocator<nc_T>().allocate(ext.size(), const_cast<nc_T*>(src), true), CLDeleter<T>()),
+    : extent(ext), cache(ext.size(), const_cast<nc_T*>(src), true),
       offset(0), extent_base(ext) {}
 
 template <typename T, int N>
@@ -658,7 +653,7 @@ void array_view<const T, N>::refresh() const {
     assert(cache.get());
     assert(extent == extent_base && "Only support non-sectioned view");
     assert(offset == 0 && "Only support non-sectioned view");
-    getAllocator().refresh(cache.get_device());
+    cache.refresh();
 }
 
 #else // GPU implementations
