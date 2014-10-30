b98ccea03b4bd82846b8fcbd4ea61ecc5b77a795
Jack Chung <jack@multicorewareinc.com>
2014-10-30 12:09:31 +0800

    [refactor] move HSA-specific codes from mcwamp.cpp to mcwamp_hsa.cpp

--- lib/mcwamp_hsa.cpp
+++ lib/mcwamp_hsa.cpp
@@ -6,3 +6,151 @@
 //===----------------------------------------------------------------------===//
 
 // FIXME this file will place C++AMP Runtime implementation (HSA version)
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include "HSAContext.h"
+
+extern "C" char * kernel_source_[] asm ("_binary_kernel_cl_start") __attribute__((weak));
+extern "C" char * kernel_size_[] asm ("_binary_kernel_cl_size") __attribute__((weak));
+
+namespace Concurrency {
+namespace CLAMP {
+
+static HSAContext *context = NULL;
+
+void FinalizeHSAContext() {
+  if (context != NULL) {
+    context->dispose();
+    context = NULL;
+  }
+
+  // TBD dispose all Kernel objects
+
+  // TBD dispose all Dispatch objects
+}
+
+/* Used only in HSA runtime */
+HSAContext *GetOrInitHSAContext(void)
+{
+  if (!context) {
+    //std::cerr << "CLAMP::HSA: create context\n";
+    context = HSAContext::Create();
+    atexit(FinalizeHSAContext); // register finalizer
+  }
+  if (!context) {
+    std::cerr << "CLAMP::HSA: Unable to create context\n";
+    abort();
+  }
+  return context;
+}
+
+static std::map<std::string, HSAContext::Kernel *> __mcw_hsa_kernels;
+void *CreateHSAKernel(std::string s)
+{
+  HSAContext::Kernel *kernel = __mcw_hsa_kernels[s];
+  if (!kernel) {
+      size_t kernel_size = (size_t)((void *)kernel_size_);
+      char *kernel_source = (char*)malloc(kernel_size+1);
+      memcpy(kernel_source, kernel_source_, kernel_size);
+      kernel_source[kernel_size] = '\0';
+      std::string kname = std::string("&")+s;
+      //std::cerr << "CLAMP::HSA::Creating kernel: " << kname << "\n";
+      kernel = GetOrInitHSAContext()->
+          createKernel(kernel_source, kernel_size, kname.c_str());
+      if (!kernel) {
+          std::cerr << "CLAMP::HSA: Unable to create kernel\n";
+          abort();
+      } else {
+          //std::cerr << "CLAMP::HSA: Created kernel\n";
+      }
+      __mcw_hsa_kernels[s] = kernel;
+  }
+
+  HSAContext::Dispatch *dispatch = GetOrInitHSAContext()->createDispatch(kernel);
+  dispatch->clearArgs();
+//#define CXXAMP_ENABLE_HSAIL_HLC_DEVELOPMENT_COMPILER 1
+#ifndef CXXAMP_ENABLE_HSAIL_HLC_DEVELOPMENT_COMPILER
+  dispatch->pushLongArg(0);
+  dispatch->pushLongArg(0);
+  dispatch->pushLongArg(0);
+  dispatch->pushLongArg(0);
+  dispatch->pushLongArg(0);
+  dispatch->pushLongArg(0);
+#endif
+  return dispatch;
+}
+
+namespace HSA {
+void RegisterMemory(void *p, size_t sz)
+{
+    //std::cerr << "registering: ptr " << p << " of size " << sz << "\n";
+    GetOrInitHSAContext()->registerArrayMemory(p, sz);
+}
+}
+
+std::future<void> HSALaunchKernelAsync(void *ker, size_t nr_dim, size_t *global, size_t *local)
+{
+  HSAContext::Dispatch *dispatch =
+      reinterpret_cast<HSAContext::Dispatch*>(ker);
+  size_t tmp_local[] = {0, 0, 0};
+  if (!local)
+      local = tmp_local;
+  //std::cerr<<"Launching: nr dim = " << nr_dim << "\n";
+  //for (size_t i = 0; i < nr_dim; ++i) {
+  //  std::cerr << "g: " << global[i] << " l: " << local[i] << "\n";
+  //}
+  dispatch->setLaunchAttributes(nr_dim, global, local);
+  //std::cerr << "Now real launch\n";
+  //kernel->dispatchKernelWaitComplete();
+
+  return dispatch->dispatchKernelAndGetFuture();
+}
+
+void HSALaunchKernel(void *ker, size_t nr_dim, size_t *global, size_t *local)
+{
+  HSAContext::Dispatch *dispatch =
+      reinterpret_cast<HSAContext::Dispatch*>(ker);
+  size_t tmp_local[] = {0, 0, 0};
+  if (!local)
+      local = tmp_local;
+  //std::cerr<<"Launching: nr dim = " << nr_dim << "\n";
+  //for (size_t i = 0; i < nr_dim; ++i) {
+  //  std::cerr << "g: " << global[i] << " l: " << local[i] << "\n";
+  //}
+  dispatch->setLaunchAttributes(nr_dim, global, local);
+  //std::cerr << "Now real launch\n";
+  dispatch->dispatchKernelWaitComplete();
+}
+
+void HSAPushArg(void *ker, size_t sz, const void *v)
+{
+  //std::cerr << "pushing:" << ker << " of size " << sz << "\n";
+  HSAContext::Dispatch *dispatch =
+      reinterpret_cast<HSAContext::Dispatch*>(ker);
+  void *val = const_cast<void*>(v);
+  switch (sz) {
+    case sizeof(double):
+      dispatch->pushDoubleArg(*reinterpret_cast<double*>(val));
+      break;
+    case sizeof(int):
+      dispatch->pushIntArg(*reinterpret_cast<int*>(val));
+      //std::cerr << "(int) value = " << *reinterpret_cast<int*>(val) <<"\n";
+      break;
+    case sizeof(unsigned char):
+      dispatch->pushBooleanArg(*reinterpret_cast<unsigned char*>(val));
+      break;
+    default:
+      assert(0 && "Unsupported kernel argument size");
+  }
+}
+void HSAPushPointer(void *ker, void *val)
+{
+    //std::cerr << "pushing:" << ker << " of ptr " << val << "\n";
+    HSAContext::Dispatch *dispatch =
+        reinterpret_cast<HSAContext::Dispatch*>(ker);
+    dispatch->pushPointerArg(val);
+}
+} // namespace CLAMP
+} // namespace Concurrency
