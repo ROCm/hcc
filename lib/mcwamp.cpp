5720f57192eacb896185cbffbf7b4c60ec7a5b3d
Jack Chung <jack@multicorewareinc.com>
2014-10-30 15:19:36 +0800

    [refactor] move Concurrency::CLAMP::MatchKernelNames from mcwamp_opencl to mcwamp
    - the routing is common in both OpenCL and HSA so in this commit we move it from mcwamp_opencl to mcwamp

--- lib/mcwamp.cpp
+++ lib/mcwamp.cpp
@@ -20,3 +20,82 @@ std::shared_ptr<accelerator> accelerator::_default_accelerator = nullptr;
 
 } // namespace Concurrency
 
+std::vector<std::string> __mcw_kernel_names;
+
+namespace Concurrency {
+namespace CLAMP {
+
+// Levenshtein Distance to measure the difference of two sequences
+// The shortest distance it returns the more likely the two sequences are equal
+static inline int ldistance(const std::string source, const std::string target)
+{
+  int n = source.length();
+  int m = target.length();
+  if (m == 0)
+    return n;
+  if (n == 0)
+    return m;
+
+  //Construct a matrix
+  typedef std::vector < std::vector < int >>Tmatrix;
+  Tmatrix matrix(n + 1);
+
+  for (int i = 0; i <= n; i++)
+    matrix[i].resize(m + 1);
+  for (int i = 1; i <= n; i++)
+    matrix[i][0] = i;
+  for (int i = 1; i <= m; i++)
+    matrix[0][i] = i;
+
+  for (int i = 1; i <= n; i++) {
+    const char si = source[i - 1];
+    for (int j = 1; j <= m; j++) {
+      const char dj = target[j - 1];
+      int cost;
+      if (si == dj)
+        cost = 0;
+      else
+        cost = 1;
+      const int above = matrix[i - 1][j] + 1;
+      const int left = matrix[i][j - 1] + 1;
+      const int diag = matrix[i - 1][j - 1] + cost;
+      matrix[i][j] = std::min(above, std::min(left, diag));
+    }
+  }
+  return matrix[n][m];
+}
+
+// transformed_kernel_name (mangled) might differ if usages of 'm32' flag in CPU/GPU
+// paths are mutually exclusive. We can scan all kernel names and replace
+// transformed_kernel_name with the one that has the shortest distance from it by using 
+// Levenshtein Distance measurement
+void MatchKernelNames(std::string& fixed_name) {
+  if (__mcw_kernel_names.size()) {
+    // Must start from a big value > 10
+    int distance = 1024;
+    int hit = -1;
+    std::string shortest;
+    for (std::vector < std::string >::iterator it = __mcw_kernel_names.begin();
+         it != __mcw_kernel_names.end(); ++it) {
+      if ((*it) == fixed_name) {
+        // Perfect match. Mark no need to replace and skip the loop
+        hit = -1;
+        break;
+      }
+      int n = ldistance(fixed_name, (*it));
+      if (n <= distance) {
+        distance = n;
+        hit = 1;
+        shortest = (*it);
+      }
+    }
+    /* Replacement. Skip if not hit or the distance is too far (>5)*/
+    if (hit >= 0 && distance < 5)
+      fixed_name = shortest;
+  }
+  return;
+}
+
+} // namespace CLAMP
+} // namespace Concurrency
+
