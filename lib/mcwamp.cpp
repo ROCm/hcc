9c1b1bc6d539383399d30ad88d797b876a4810b6
UncleHandsome <yunwei@multicorewareinc.com>
2015-02-11 12:01:35 +0800

    [Refractor] Move atomic functions to a separate file

--- lib/mcwamp.cpp
+++ lib/mcwamp.cpp
@@ -27,40 +27,6 @@ std::shared_ptr<accelerator> accelerator::_gpu_accelerator = std::make_shared<ac
 std::shared_ptr<accelerator> accelerator::_cpu_accelerator = std::make_shared<accelerator>(accelerator::cpu_accelerator);
 std::shared_ptr<accelerator> accelerator::_default_accelerator = nullptr;
 
-std::mutex afa_u, afa_i;
-unsigned int atomic_add_unsigned(unsigned int *x, unsigned int y) {
-    std::lock_guard<std::mutex> guard(afa_u);
-    *x += y;
-    return *x;
-}
-int atomic_add_int(int *x, int y) {
-    std::lock_guard<std::mutex> guard(afa_i);
-    *x += y;
-    return *x;
-}
-std::mutex afm_u, afm_i;
-unsigned int atomic_max_unsigned(unsigned int *p, unsigned int val) {
-    std::lock_guard<std::mutex> guard(afm_u);
-    *p = std::max(*p, val);
-    return *p;
-}
-int atomic_max_int(int *p, int val) {
-    std::lock_guard<std::mutex> guard(afm_i);
-    *p = std::max(*p, val);
-    return *p;
-}
-std::mutex afi_u, afi_i;
-unsigned int atomic_inc_unsigned(unsigned int *p) {
-    std::lock_guard<std::mutex> guard(afi_u);
-    *p += 1;
-    return *p;
-}
-int atomic_inc_int(int *p) {
-    std::lock_guard<std::mutex> guard(afi_i);
-    *p += 1;
-    return *p;
-}
-
 } // namespace Concurrency
 
 std::vector<std::string> __mcw_kernel_names;
