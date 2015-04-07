20d4694dab299fcdb8756aaa26fcfea8d2a9ba82
UncleHandsome <yunwei@multicorewareinc.com>
2015-04-07 22:35:03 +0800

    Add test cases

---
+++ stl-test/Tests/coordinate/bounds/iterators.cpp
@@ -0,0 +1,16 @@
+#include <coordinate>
+int main()
+{
+    bool ret = true;
+    {
+        std::bounds<4> a({5, 5, 6, 6});
+        auto it = a.begin();
+        ret &= *it == std::index<4>{};
+    }
+    {
+        std::bounds<4> a({0, 5, 6, 6});
+        auto it = a.begin();
+        ret &= it == a.end();
+    }
+    return !ret;
+}
---
+++ stl-test/Tests/coordinate/bounds_iterator/test.cpp
@@ -0,0 +1,20 @@
+#include <coordinate>
+#include <stdio.h>
+int main()
+{
+    bool ret = true;
+    {
+        std::bounds<4> a({5, 5, 6, 6});
+        auto it1 = a.begin();
+        std::bounds<4> b({5, 5, 6, 6});
+        auto it2 = b.begin();
+        ret &= it1 == it2;
+        ret &= it1 <= it2;
+        ret &= it1 >= it2;
+        it2++;
+        ret &= *it2 == std::index<4>({1, 0, 0, 0});
+        ret &= it1 < it2;
+        ret &= it2 > it1;
+    }
+    return !ret;
+}
