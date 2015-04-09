606e01b44a0b29b0f8767c970d51b616c8eca428
UncleHandsome <yunwei@multicorewareinc.com>
2015-04-09 15:56:27 +0800

    strength test cases

--- stl-test/Tests/array_view/array_view.cpp
+++ stl-test/Tests/array_view/array_view.cpp
@@ -1,6 +1,6 @@
 #include <array_view>
 #include <vector>
-#include <stdio.h>
+#include <string>
 int main()
 {
     bool ret = true;
@@ -14,6 +14,8 @@ int main()
         std::array_view<int, 1> av(vec); 
         ret &= av.size() == vec.size();
         ret &= av.data() == vec.data();
+        for (int i = 0; i < 120; i++)
+            ret &= av.data()[i] == i;
     }
     {
         std::vector<int> vec(120);
@@ -23,6 +25,8 @@ int main()
         std::array_view<int, 1> bv(av); 
         ret &= av.size() == bv.size();
         ret &= av.data() == bv.data();
+        for (int i = 0; i < 120; i++)
+            ret &= bv.data()[i] == i;
     }
     {
         char a[3][1][4] {{{'H', 'i'}}};
@@ -39,19 +43,23 @@ int main()
         std::array_view<const int, 3> bv(av); 
         ret &= av.size() == bv.size();
         ret &= av.data() == bv.data();
-        ret &= bv[{1, 2, 3}] == 63;
-        ret &= bv[{2, 2, 3}] == 103;
+        for (int i = 0; i < 120; i++)
+            ret &= bv.data()[i] == i;
     }
     {
         std::vector<int> vec(120);
         for (int i = 0; i < 120; i++)
             vec[i] = i;
         std::array_view<int, 3> av(vec, std::bounds<3>{3, 4, 10}); 
-        std::array_view<int, 3> bv(av, std::bounds<3>{6, 4, 5}); 
+        std::array_view<int, 3> bv(av, std::bounds<3>{6, 10, 2}); 
         ret &= av.size() == bv.size();
         ret &= av.data() == bv.data();
-        ret &= av[{1, 2, 3}] == 63;
-        ret &= av[{2, 2, 3}] == 103;
+        for (int i = 0; i < 120; i++)
+            ret &= bv.data()[i] == i;
+        for (int i = 0; i < 6; i++)
+            for (int j = 0; j < 10; j++)
+                for (int k = 0; k < 2; k++)
+                    ret &= bv[{i, j, k}] == i * 20 + j * 2 + k;
     }
     {
         std::vector<int> vec(120);
@@ -61,16 +69,21 @@ int main()
         ret &= av.size() == vec.size();
         ret &= av.data() == vec.data();
         ret &= av.bounds() == std::bounds<3>{3, 4, 10};
-        ret &= av[{1, 2, 3}] == 63;
-        ret &= av[{2, 2, 3}] == 103;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 4; j++)
+                for (int k = 0; k < 10; k++)
+                    ret &= av[{i, j, k}] == i * 40 + j * 10 + k;
+ 
     }
     {
         std::vector<int> vec(120);
         for (int i = 0; i < 120; i++)
             vec[i] = i;
         std::array_view<int, 3> av(vec.data(), std::bounds<3>{3, 4, 10});
-        ret &= av[1][2][3] == 63;
-        ret &= av[2][2][3] == 103;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 4; j++)
+                for (int k = 0; k < 10; k++)
+                    ret &= av[i][j][k] == i * 40 + j * 10 + k;
     }
     {
         std::vector<int> vec(120);
