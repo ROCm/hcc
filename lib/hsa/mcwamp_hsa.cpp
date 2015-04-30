d44c281bc42e77f3d975f4238ce8fdc57a5e4f0f
UncleHandsome <yunwei@multicorewareinc.com>
2015-04-30 15:57:31 +0800

    Remove read and write

--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -222,11 +222,9 @@ extern "C" void LaunchKernelImpl(void *ker, size_t nr_dim, size_t *global, size_
   //  std::cerr << "g: " << global[i] << " l: " << local[i] << "\n";
   //}
   Concurrency::HSAAMPAllocator& aloc = Concurrency::getHSAAMPAllocator();
-  aloc.write();
   dispatch->setLaunchAttributes(nr_dim, global, local);
   //std::cerr << "Now real launch\n";
   dispatch->dispatchKernelWaitComplete();
-  aloc.read();
 }
 
 extern "C" void *LaunchKernelAsyncImpl(void *ker, size_t nr_dim, size_t *global, size_t *local)
@@ -242,7 +240,6 @@ extern "C" void *LaunchKernelAsyncImpl(void *ker, size_t nr_dim, size_t *global,
   //  std::cerr << "g: " << global[i] << " l: " << local[i] << "\n";
   //}
   Concurrency::HSAAMPAllocator& aloc = Concurrency::getHSAAMPAllocator();
-  aloc.write();
   dispatch->setLaunchAttributes(nr_dim, global, local);
   std::shared_future<void>* fut = dispatch->dispatchKernelAndGetFuture();
   // FIXME what about aloc.read() ??
