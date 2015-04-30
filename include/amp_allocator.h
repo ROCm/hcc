9e9e35c912b98c90a2ac1237e10afe8741ce191c
UncleHandsome <yunwei@multicorewareinc.com>
2015-04-30 20:07:01 +0800

    Implement Template Method

--- include/amp_allocator.h
+++ include/amp_allocator.h
@@ -3,19 +3,105 @@
 #include <serialize.h>
 namespace Concurrency {
 
+struct rw_info
+{
+    bool discard;
+    bool dirty;
+    bool del;
+};
+
 class AMPAllocator {
 public:
-  AMPAllocator() {}
   virtual ~AMPAllocator() {}
-  virtual void *init(int, void*) = 0;
-  virtual void append(void*, int, void*, bool) = 0;
-  virtual void sync(void*) = 0;
-  virtual void* device_data(void*) = 0;
-  virtual void copy(void* dst, void* src, size_t count) = 0;
-  virtual void discard(void*) = 0;
-  virtual void stash(void*) = 0;
-  virtual void* getQueue() = 0;
-  virtual void free(void*) = 0;
+
+  void *init(int count, void* data) {
+      if (count > 0) {
+          if (data == nullptr) {
+              data = aligned_alloc(0x1000, count);
+              rwq[data] = {false, false, true};
+          } else
+              rwq[data] = {false, false, false};
+          regist(count, data);
+      }
+      return data;
+  }
+
+  void append(void* kernel, int idx, void* data, bool isArray) {
+      PushArg(kernel, idx, data);
+      auto it = rwq.find(data);
+      if (it != std::end(rwq)) {
+          rw_info& rw = it->second;
+          if (!rw.dirty) {
+              rw.dirty = true;
+              if (!rw.discard || isArray)
+                  amp_write(data);
+              rw.discard = false;
+          }
+      }
+  }
+
+  void discard(void *data) {
+      auto it = rwq.find(data);
+      if (it != std::end(rwq)) {
+          it->second.discard = true;
+          it->second.dirty = false;
+      }
+  }
+
+  void stash(void *data) {
+      auto it = rwq.find(data);
+      if (it != std::end(rwq))
+          it->second.dirty = false;
+  }
+
+  void copy(void* dst, void* src, size_t count) {
+      auto it = rwq.find(src);
+      if (it != std::end(rwq) && it->second.dirty)
+          amp_copy(dst, src, count);
+      else
+          memmove(dst, src, count);
+  }
+
+  void sync(void* data) {
+      auto it = rwq.find(data);
+      if (it != std::end(rwq)) {
+          rw_info& rw = it->second;
+          if (rw.dirty && !rw.discard) {
+              amp_read(data);
+              rw.dirty = false;
+          }
+      }
+  }
+
+  void free(void* data) {
+      auto it = rwq.find(data);
+      if (it != std::end(rwq)) {
+          rw_info& rw = it->second;
+          if (rw.del)
+              ::operator delete(data);
+          else
+              sync(data);
+          rwq.erase(it);
+      }
+      unregist(data);
+  }
+
+
+  void* device_data(void* data) { return _device_data(data); }
+  void* getQueue() { return _getQueue(); }
+private:
+
+  std::map<void *, rw_info> rwq;
+
+  virtual void* _getQueue() { return nullptr; }
+  virtual void* _device_data(void *data) { return nullptr; }
+  // overide function
+  virtual void regist(int count, void *data) {};
+  virtual void PushArg(void* kernel, int idx, void* data) {}
+  virtual void amp_write(void *data) {};
+  virtual void amp_read(void *data) {}
+  virtual void amp_copy(void *dst, void *src, int n) {};
+  virtual void unregist(void *data) {};
 };
 
 AMPAllocator *getAllocator();
