4553c07fc63c4443c216c8b1d82660e76884f25d
Jack Chung <jack@multicorewareinc.com>
2015-06-10 15:49:30 +0800

    [HSA] revise test case for HSA printf
    In this new commit, different workitems would print different outputs,
    and invoke hsa_printf() for different times.

--- tests/Unit/HSA/printf.cpp
+++ tests/Unit/HSA/printf.cpp
@@ -4,8 +4,13 @@
 #include <amp.h>
 #include <hsa_printf.h>
 
+#include <iostream>
+
+#define SIZE (32)
+#define PRINTF_BUFFER_SIZE (512)
+
 int main() {
-  HSAPrintfPacketQueue* q = createHSAPrintfPacketQueue(32);
+  HSAPrintfPacketQueue* q = createHSAPrintfPacketQueue(PRINTF_BUFFER_SIZE);
   
   using namespace concurrency;
 
@@ -14,20 +19,23 @@ int main() {
   const char* s3 = "work-item";
   const char* s4 = "work item";
   const char* s5 = "workitem";
-
-#define SIZE (32)
+  const char* s6 = "Hello again from %s: %d\n";
 
   parallel_for_each(extent<1>(SIZE), [&](index<1> idx) restrict(amp) {
     if (idx[0] == 0) {
       hsa_printf(q, s1, s2, idx[0]);
+      hsa_printf(q, s6, s2, idx[0]);
     } else if (idx[0] == 5) {
       hsa_printf(q, s1, s3, idx[0]);
     } else if (idx[0] == 10) {
       hsa_printf(q, s1, s4, idx[0]);
     } else if (idx[0] == 15) {
       hsa_printf(q, s1, s5, idx[0]);
+      hsa_printf(q, s6, s2, idx[0]);
     }
   });
+
+  dumpHSAPrintfPacketQueue(q);
 
   hsa_process_printf_queue(q);
 
