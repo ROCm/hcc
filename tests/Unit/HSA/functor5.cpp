6b2912fd166608363273704a8cab6890faa7c4bb
Jack Chung <jack@multicorewareinc.com>
2015-06-15 11:34:41 +0800

    [test] add HSA test cases where functors are supplied to parallel_for_each
    Up until now all previous tests check lambdas passed parallel_for_each.
    In this commit we add tests which test if functors are supplied to
    parallel_for_each.

---
+++ tests/Unit/HSA/functor5.cpp
@@ -0,0 +1,81 @@
+// XFAIL: Linux
+// RUN: %cxxamp -Xclang -fhsa-ext %s -o %t.out && %t.out
+
+#include <amp.h>
+
+#include <iostream>
+
+#define SIZE (16)
+
+using namespace concurrency;
+
+// test supply a class with operator() to parallel_for_each
+// the template class will call a separate template functor
+
+template<typename _Tp, size_t N>
+class user_functor {
+public:
+  void operator() (index<1>& idx, _Tp (&input)[N]) restrict(amp) {
+    input[idx[0]] = idx[0];
+  }
+};
+
+template<typename _Tp, size_t N>
+class prog {
+  _Tp (&input)[N];
+  user_functor<_Tp, N>& kernel;
+
+public:
+  prog(_Tp (&t)[N], user_functor<_Tp, N>& f) : input(t), kernel(f) {
+    run();
+  }
+
+  void operator() (index<1>& idx) restrict(amp) {
+    kernel(idx, input);
+  }
+
+  void run() {
+    parallel_for_each(extent<1>(N), *this);
+  }
+
+  // verify output
+  bool test() {
+    bool ret = true;
+    for (int i = 0; i < N; ++i) {
+      if (input[i] != i) {
+        ret = false;
+        break;
+      }
+    }
+    return true;
+  }
+};
+
+int main() {
+  bool ret = true;
+ 
+  // prepare test data
+  int input_int[SIZE] { 0 };
+  unsigned input_unsigned[SIZE] { 0 };
+  float input_float[SIZE] { 0 };
+  double input_double[SIZE] { 0 };
+
+  // launch kernel
+  user_functor<int, SIZE>      kernel_int;
+  user_functor<unsigned, SIZE> kernel_unsigned;
+  user_functor<float, SIZE>    kernel_float;
+  user_functor<double, SIZE>   kernel_double;
+  prog<int, SIZE>      p1(input_int, kernel_int);
+  prog<unsigned, SIZE> p2(input_unsigned, kernel_unsigned);
+  prog<float, SIZE>    p3(input_float, kernel_float);
+  prog<double, SIZE>   p4(input_double, kernel_double);
+
+  // check result
+  ret &= p1.test();
+  ret &= p2.test();
+  ret &= p3.test();
+  ret &= p4.test();
+
+  return !(ret == true);
+}
+
