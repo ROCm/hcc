9490ecfa41034aa3fe1e9a0c373ad2158593e7ca
UncleHandsome <yunweilee@multicorewareinc.com>
2015-06-22 22:15:07 -0500

    Add comment

--- include/amp_runtime.h
+++ include/amp_runtime.h
@@ -35,6 +35,7 @@ private:
 
 
 
+/// forward declaration
 class KalmarDevice;
 struct rw_info;
 
@@ -52,6 +53,9 @@ enum queuing_mode {
     queuing_mode_automatic
 };
 
+/// KalmarQueue
+/// This is the base implementation of accelerator_view
+/// KalamrQueue is responsible for data operation and launch kernel
 class KalmarQueue
 {
 public:
@@ -67,7 +71,7 @@ public:
   /// wrtie data from host to device
   virtual void write(void* device, const void* src, size_t count, size_t offset, bool blocking, bool free) = 0;
 
-  /// copy data between device pointer
+  /// copy data between two device pointers
   virtual void copy(void* src, void* dst, size_t count, size_t src_offset, size_t dst_offset, bool blocking) = 0;
 
   /// map host accessible pointer from device
@@ -75,7 +79,7 @@ public:
 
   /// unmap host accessible pointer
   virtual void unmap(void* device, void* addr) = 0;
-  
+
   /// push device pointer to kernel argument list
   virtual void Push(void *kernel, int idx, void* device, bool isConst) = 0;
 
@@ -89,11 +93,16 @@ private:
   queuing_mode mode;
 };
 
+/// KalmarDevice
+/// This is the base implementation of accelerator
+/// KalmarDevice is responsible for create/release memory on device
 class KalmarDevice
 {
 private:
     access_type cpu_type;
+    /// default KalmarQueue
     std::shared_ptr<KalmarQueue> def;
+    /// make sure KalamrQueue is created only once
     std::once_flag flag;
 protected:
     KalmarDevice(access_type type = access_type_read_write) : cpu_type(type), def(), flag() {}
@@ -110,21 +119,22 @@ public:
     virtual bool is_emulated() const = 0;
 
 
-    /// create buffer on device
-    /// @key on accelerator that supports shared memory
+    /// create buffer
+    /// @key on device that supports shared memory
     //       key can used to avoid duplicate allocation
     virtual void* create(size_t count, struct rw_info* key) = 0;
 
-    /// release buffer on device
+    /// release buffer
+    /// @key: used to avoid duplicate release
     virtual void release(void* ptr, struct rw_info* key) = 0;
 
-    /// create kernel for current device
+    /// create kernel
     virtual void* CreateKernel(const char* fun, void* size, void* source) { return nullptr; }
 
     /// check the dimension information is correct
     virtual bool check(size_t* size, size_t dim_ext) { return true; }
 
-    /// create work queue from current device
+    /// create KalmarQueue from current device
     virtual std::shared_ptr<KalmarQueue> createQueue() = 0;
     virtual ~KalmarDevice() {}
 
@@ -166,6 +176,7 @@ public:
   void Push(void *kernel, int idx, void* device, bool isConst) override {}
 };
 
+/// cpu accelerator
 class CPUDevice final : public KalmarDevice
 {
 public:
@@ -184,6 +195,9 @@ public:
     void* CreateKernel(const char* fun, void* size, void* source) { return nullptr; }
 };
 
+/// KalmarContext
+/// This is responsible for managing all devices
+/// User will need to add their customize devices
 class KalmarContext
 {
 protected:
@@ -269,7 +283,7 @@ enum states
     invalid // not able to read and write on the data, need to read from other device
 };
 
-/// store information of each device
+/// buffer information
 struct dev_info
 {
     void* data; /// pointer to device data
@@ -303,16 +317,22 @@ static inline void copy_helper(std::shared_ptr<KalmarQueue>& srcQueue, dev_info&
 
 struct rw_info
 {
-    /// host accessible pointer
-    /// it will be set if
-    /// 1. rw_info constructed on cpu accelerator
-    /// 2. rw_info constructed on accelerator supports
+    /// host accessible pointer, it will be set if
+    /// 1. rw_info constructed by cpu accelerator
+    /// 2. rw_info constructed by accelerator supports
     ///    unified memory and access_type is not none
     void *data;
     const size_t count;
+    /// This pointer pointes to the queue that contains the latest data
     std::shared_ptr<KalmarQueue> curr;
+    /// This pointer pointes to the queue that used to construct this rw_info
+    /// This will be null if the constructor is constructed by size only
     std::shared_ptr<KalmarQueue> master;
+    /// staged queue
     std::shared_ptr<KalmarQueue> stage;
+    /// This is used as cache for device buffer
+    /// When this rw_info is going to be used(computed) on device,
+    /// rw_info will allocate buffer for the device
     std::map<KalmarDevice*, dev_info> devs;
     access_type mode;
     /// This will be set if this rw_info is constructed with host pointer
@@ -320,7 +340,11 @@ struct rw_info
     unsigned int HostPtr : 1;
 
 
-    // consruct array_view
+    /// consruct array_view
+    /// From standard, array_view will be constructed by size, or size with
+    /// host pointer.
+    /// If it is constructed by host pointer, treat it is constructed on cpu
+    /// device, set the HostPtr flag to prevent destructor to release it
     rw_info(const size_t count, void* ptr)
         : data(ptr), count(count), curr(nullptr), master(nullptr), stage(nullptr),
         devs(), mode(access_type_none), HostPtr(ptr != nullptr) {
@@ -339,7 +363,12 @@ struct rw_info
             }
         }
 
-    // construct array
+    /// construct array
+    /// From AMP standard, array should be constructed with
+    /// 1. one accelerator_view
+    /// 2. one acceleratir_view, with another staged one
+    ///    In this case, master should be cpu device
+    ///    If it is not, ignore the stage one, fallback to case 1.
     rw_info(const std::shared_ptr<KalmarQueue> Queue, const std::shared_ptr<KalmarQueue> Stage,
             const size_t count, access_type mode_) : data(nullptr), count(count),
     curr(Queue), master(Queue), stage(nullptr), devs(), mode(mode_), HostPtr(false) {
@@ -353,7 +382,7 @@ struct rw_info
             mode = curr->getDev()->get_access();
         devs[curr->getDev()] = {curr->getDev()->create(count, this), modified};
 
-        // set data pointer
+        /// set data pointer
         if (is_cpu_dev(curr) || (curr->getDev()->is_unified() && mode != access_type_none))
             data = devs[curr->getDev()].data;
         if (is_cpu_dev(curr)) {
@@ -376,9 +405,9 @@ struct rw_info
             it.second.state = invalid;
     }
 
-    /// optimization: Before performing copy, if the state of cpu accelerator is shared,
-    /// it means the data on cpu is the ssame on curr accelerator, use the data
-    /// on cpu to perform the copy
+    /// optimization: Before performing copy, if the state of cpu accelerator is
+    /// shared, it implies that the data on cpu is the same on device where
+    /// curr located, it is safe to use the data on cpu to perform the later operation
     void try_switch_to_cpu() {
         if (is_cpu_dev(curr))
             return;
@@ -388,7 +417,11 @@ struct rw_info
                 curr = cpu_queue;
     }
 
-    /// synchronize data to device pQueue belongs to
+    /// synchronize data to device pQueue belongs to by using pQuquq
+    /// @pQueue: queue that used to synchronize
+    /// @modify:
+    /// @blcok: this call should act blocking or not
+    //          non-blocking call will occur in serilization stage
     void sync(std::shared_ptr<KalmarQueue> pQueue, bool modify, bool block = true) {
 #if __KALMAR_ACCELERATOR__ == 2 || __KALMAR_CPU__ == 2
         if (CLAMP::in_cpu_kernel())
@@ -451,9 +484,15 @@ struct rw_info
         }
     }
 
+    /// return a host accessible pointer from device
+    /// @cnt: size to map
+    /// @offset: offset to map
+    /// @modify: change state if it is going to be modified
     void* map(size_t cnt, size_t offset, bool modify) {
         if (cnt == 0)
             cnt = count;
+        /// This can only happen if this rw_info is constructed only with size
+        /// and not accessed on any device
         if (!curr) {
             curr = getContext()->auto_select();
             devs[curr->getDev()] = {curr->getDev()->create(count, this), modify ? modified : shared};
@@ -469,9 +508,16 @@ struct rw_info
     }
 
     void unmap(void* addr) { curr->unmap(devs[curr->getDev()].data, addr); }
+
+    /// synchronize data to master accelerator
+    /// used in array
     void synchronize(bool modify) { sync(master, modify); }
+
+    /// synchronize data to cpu accelerator
+    /// used in array_view
     void get_cpu_access(bool modify) { sync(get_cpu_queue(), modify); }
 
+    /// Write data from host source pointer
     void write(const void* src, int cnt, int offset, bool blocking) {
         curr->write(devs[curr->getDev()].data, src, cnt, offset, blocking, false);
         dev_info& dev = devs[curr->getDev()];
@@ -481,10 +527,12 @@ struct rw_info
         }
     }
 
+    /// Read data to host pointer
     void read(void* dst, int cnt, int offset) {
         curr->read(devs[curr->getDev()].data, dst, cnt, offset);
     }
 
+    /// copy data from "this" to other
     void copy(rw_info* other, int src_offset, int dst_offset, int cnt) {
         if (cnt == 0)
             cnt = count;
@@ -499,6 +547,7 @@ struct rw_info
         }
         dev_info& dst = other->devs[other->curr->getDev()];
         dev_info& src = devs[curr->getDev()];
+        /// If src.state is invalid, zero the data on it
         if (src.state == invalid) {
             src.state = shared;
             if (is_cpu_dev(curr))
@@ -523,13 +572,16 @@ struct rw_info
             return;
         }
 #endif
+        /// If this rw_info is constructed by host pointer
+        /// 1. synchronize latest data to host pointer
+        /// 2. Because the data pointer cannout be released, erase itself from devs
         if (HostPtr)
             synchronize(false);
-        auto cpu_queue = get_cpu_queue()->getDev();
-        if (devs.find(cpu_queue) != std::end(devs)) {
+        auto cpu_dev = get_cpu_queue()->getDev();
+        if (devs.find(cpu_dev) != std::end(devs)) {
             if (!HostPtr)
-                cpu_queue->release(devs[cpu_queue].data, this);
-            devs.erase(cpu_queue);
+                cpu_dev->release(devs[cpu_dev].data, this);
+            devs.erase(cpu_dev);
         }
         KalmarDevice* pDev;
         dev_info info;
