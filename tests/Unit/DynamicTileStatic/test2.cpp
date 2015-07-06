39051f31f36563bead7b0028da372bd05c5a79c7
Jack Chung <jack@multicorewareinc.com>
2015-07-06 17:55:38 +0800

    [HC test] revise test case to use tiled_extent_2D
    Unlike tiled_extent in C++AMP, tiled_extent_2D allows tile size be determined at runtime.
    In this commit we revise the test case to check various constructors of tiled_extent_2D.

--- tests/Unit/DynamicTileStatic/test2.cpp
+++ tests/Unit/DynamicTileStatic/test2.cpp
@@ -60,11 +60,80 @@ bool test1D() {
   return ret;
 }
 
+/// test tiled_extent_2D
+bool test2D() {
+  bool ret = true;
+
+  using namespace concurrency;
+
+  // test default constructor
+  tiled_extent_2D tile1;
+
+  ret &= (tile1.tile_dim0 == 0);
+  ret &= (tile1.tile_dim1 == 0);
+  ret &= (tile1[0] == 0);
+  ret &= (tile1[1] == 0);
+
+  // test constructor (int, int, int, int)
+  tiled_extent_2D tile2(1, 1, 1, 1);
+
+  ret &= (tile2.tile_dim0 == 1);
+  ret &= (tile2.tile_dim1 == 1);
+  ret &= (tile2[0] == 1);
+  ret &= (tile2[1] == 1);
+
+  tiled_extent_2D tile3(64, 16, 8, 4);
+
+  ret &= (tile3.tile_dim0 == 8);
+  ret &= (tile3.tile_dim1 == 4);
+  ret &= (tile3[0] == 64);
+  ret &= (tile3[1] == 16);
+
+  // test copy consutrctor
+  tiled_extent_2D tile4(tile3);
+
+  ret &= (tile4.tile_dim0 == 8);
+  ret &= (tile4.tile_dim1 == 4);
+  ret &= (tile4[0] == 64);
+  ret &= (tile4[1] == 16);
+
+  // test constructor (extent<2>&, int, int)
+  extent<2> e1(16, 16);
+  tiled_extent_2D tile5(e1, 4, 4);
+
+  ret &= (tile5.tile_dim0 == 4);
+  ret &= (tile5.tile_dim1 == 4);
+  ret &= (tile5[0] == 16);
+  ret &= (tile5[1] == 16);
+
+  // setup RNG
+  std::random_device rd;
+  std::default_random_engine gen(rd());
+  std::uniform_int_distribution<int> dis(1, 1024);
+
+  // test create tiled_extent_2D where tile size is determined at runtime
+  for (int i = 0; i < 10; ++i) {
+    int extentSize0 = dis(gen);
+    int extentSize1 = dis(gen);
+    int tileSize0 = dis(gen);
+    int tileSize1 = dis(gen);
+
+    tiled_extent_2D tile6(extentSize0, extentSize1, tileSize0, tileSize1);
+
+    ret &= (tile6.tile_dim0 == tileSize0);
+    ret &= (tile6.tile_dim1 == tileSize1);
+    ret &= (tile6[0] == extentSize0);
+    ret &= (tile6[1] == extentSize1);
+  }
+
+  return ret;
+}
 /// a test which checks if tiled_extent_1D is implemented correctly
 int main() {
   bool ret = true;
 
   ret &= test1D();
+  ret &= test2D();
 
   return !(ret == true);
 }
