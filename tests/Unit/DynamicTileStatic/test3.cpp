caea314c31bc9b5be8a4198505c1a6a843133ed8
Jack Chung <jack@multicorewareinc.com>
2015-07-06 20:09:19 +0800

    [HC test] add a new test case for the new HC parallel_for_each interface
    In this commit a new test is introduced to test the following new
    parallel_for_each interface:
    
    namespace Concurrency {
    template<typename Kernel>
    void parallel_for_each(const tiled_extent_3D&, size_t, const Kernel&);
    
    template<typename Kernel>
    void parallel_for_each(const accelerator_view&, const tiled_extent_3D&,
                           size_t, const Kernel&);
    }
    
    Right now the test checks if kernel would be correctly produced based on tile
    size determined at runtime, and if work item indices are correct.
    
    Please notice in the long run we want to migrate the interface to namespace hc

--- tests/Unit/DynamicTileStatic/test3.cpp
+++ tests/Unit/DynamicTileStatic/test3.cpp
@@ -220,6 +220,166 @@ bool test2D() {
   return ret;
 }
 
+/// test HC parallel_for_each interface
+template<size_t grid_size_0, size_t grid_size_1, size_t grid_size_2, size_t tile_size_0, size_t tile_size_1, size_t tile_size_2>
+bool test3D() {
+
+  bool ret = true;
+
+  // first run normal C++AMP parallel_for_each
+
+  using namespace Concurrency;
+
+  std::vector<int> table1(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table2(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table3(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table4(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table5(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table6(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table7(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table8(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table9(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table10(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table11(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table12(grid_size_0 * grid_size_1 * grid_size_2);
+  array_view<int, 3> av1(grid_size_0, grid_size_1, grid_size_2, table1);
+  array_view<int, 3> av2(grid_size_0, grid_size_1, grid_size_2, table2);
+  array_view<int, 3> av3(grid_size_0, grid_size_1, grid_size_2, table3);
+  array_view<int, 3> av4(grid_size_0, grid_size_1, grid_size_2, table4);
+  array_view<int, 3> av5(grid_size_0, grid_size_1, grid_size_2, table5);
+  array_view<int, 3> av6(grid_size_0, grid_size_1, grid_size_2, table6);
+  array_view<int, 3> av7(grid_size_0, grid_size_1, grid_size_2, table7);
+  array_view<int, 3> av8(grid_size_0, grid_size_1, grid_size_2, table8);
+  array_view<int, 3> av9(grid_size_0, grid_size_1, grid_size_2, table9);
+  array_view<int, 3> av10(grid_size_0, grid_size_1, grid_size_2, table10);
+  array_view<int, 3> av11(grid_size_0, grid_size_1, grid_size_2, table11);
+  array_view<int, 3> av12(grid_size_0, grid_size_1, grid_size_2, table12);
+
+  parallel_for_each(extent<3>(grid_size_0, grid_size_1, grid_size_2).tile<tile_size_0, tile_size_1, tile_size_2>(), [=](tiled_index<tile_size_0, tile_size_1, tile_size_2>& idx) restrict(amp) {
+    av1(idx) = idx.global[0];
+    av2(idx) = idx.global[1];
+    av3(idx) = idx.global[2];
+    av4(idx) = idx.local[0];
+    av5(idx) = idx.local[1];
+    av6(idx) = idx.local[2];
+    av7(idx) = idx.tile[0];
+    av8(idx) = idx.tile[1];
+    av9(idx) = idx.tile[2];
+    av10(idx) = idx.tile_origin[0];
+    av11(idx) = idx.tile_origin[1];
+    av12(idx) = idx.tile_origin[2];
+  });
+
+  // next run HC parallel_for_each
+
+  std::vector<int> table13(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table14(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table15(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table16(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table17(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table18(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table19(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table20(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table21(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table22(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table23(grid_size_0 * grid_size_1 * grid_size_2);
+  std::vector<int> table24(grid_size_0 * grid_size_1 * grid_size_2);
+  array_view<int, 3> av13(grid_size_0, grid_size_1, grid_size_2, table13);
+  array_view<int, 3> av14(grid_size_0, grid_size_1, grid_size_2, table14);
+  array_view<int, 3> av15(grid_size_0, grid_size_1, grid_size_2, table15);
+  array_view<int, 3> av16(grid_size_0, grid_size_1, grid_size_2, table16);
+  array_view<int, 3> av17(grid_size_0, grid_size_1, grid_size_2, table17);
+  array_view<int, 3> av18(grid_size_0, grid_size_1, grid_size_2, table18);
+  array_view<int, 3> av19(grid_size_0, grid_size_1, grid_size_2, table19);
+  array_view<int, 3> av20(grid_size_0, grid_size_1, grid_size_2, table20);
+  array_view<int, 3> av21(grid_size_0, grid_size_1, grid_size_2, table21);
+  array_view<int, 3> av22(grid_size_0, grid_size_1, grid_size_2, table22);
+  array_view<int, 3> av23(grid_size_0, grid_size_1, grid_size_2, table23);
+  array_view<int, 3> av24(grid_size_0, grid_size_1, grid_size_2, table24);
+
+  // set dynamic tile size as 0 for now as we don't test this feature in this test yet
+  parallel_for_each(tiled_extent_3D(grid_size_0, grid_size_1, grid_size_2, tile_size_0, tile_size_1, tile_size_2), 0, [=](tiled_index_3D& idx) restrict(amp) {
+    av13(idx) = idx.global[0];
+    av14(idx) = idx.global[1];
+    av15(idx) = idx.global[2];
+    av16(idx) = idx.local[0];
+    av17(idx) = idx.local[1];
+    av18(idx) = idx.local[2];
+    av19(idx) = idx.tile[0];
+    av20(idx) = idx.tile[1];
+    av21(idx) = idx.tile[2];
+    av22(idx) = idx.tile_origin[0];
+    av23(idx) = idx.tile_origin[1];
+    av24(idx) = idx.tile_origin[2];
+  });
+
+#define SHOW_CONTENT_3D(str,av,table) \
+  { \
+    std::cout << str << "\n"; \
+    av.synchronize(); \
+    for (int i = 0; i < grid_size_0 * grid_size_1 * grid_size_2; ++i) { \
+      std::cout << table[i] << " "; \
+    } \
+    std::cout << "\n"; \
+  } \
+
+#if 0
+  SHOW_CONTENT_3D("global", av1, table1)
+  SHOW_CONTENT_3D("global", av2, table2)
+  SHOW_CONTENT_3D("global", av3, table3)
+  SHOW_CONTENT_3D("local", av4, table4)
+  SHOW_CONTENT_3D("local", av5, table5)
+  SHOW_CONTENT_3D("local", av6, table6)
+  SHOW_CONTENT_3D("tile", av7, table7)
+  SHOW_CONTENT_3D("tile", av8, table8)
+  SHOW_CONTENT_3D("tile", av9, table9)
+  SHOW_CONTENT_3D("tile_origin", av10, table10)
+  SHOW_CONTENT_3D("tile_origin", av11, table11)
+  SHOW_CONTENT_3D("tile_origin", av12, table12)
+#endif
+
+#if 0
+  SHOW_CONTENT_3D("global", av13, table13)
+  SHOW_CONTENT_3D("global", av14, table14)
+  SHOW_CONTENT_3D("global", av15, table15)
+  SHOW_CONTENT_3D("local", av16, table16)
+  SHOW_CONTENT_3D("local", av17, table17)
+  SHOW_CONTENT_3D("local", av18, table18)
+  SHOW_CONTENT_3D("tile", av19, table19)
+  SHOW_CONTENT_3D("tile", av20, table20)
+  SHOW_CONTENT_3D("tile", av21, table21)
+  SHOW_CONTENT_3D("tile_origin", av22, table22)
+  SHOW_CONTENT_3D("tile_origin", av23, table23)
+  SHOW_CONTENT_3D("tile_origin", av24, table24)
+#endif
+
+#define VERIFY_CONTENT_3D(av1, table1, av2, table2) \
+  { \
+    av1.synchronize(); av2.synchronize(); \
+    for (int i = 0; i < grid_size_0 * grid_size_1 * grid_size_2; ++i) { \
+      if (table1[i] != table2[i]) { \
+        ret = false; \
+        break; \
+      } \
+    } \
+  }
+
+  VERIFY_CONTENT_3D(av1, table1, av13, table13)
+  VERIFY_CONTENT_3D(av2, table2, av14, table14)
+  VERIFY_CONTENT_3D(av3, table3, av15, table15)
+  VERIFY_CONTENT_3D(av4, table4, av16, table16)
+  VERIFY_CONTENT_3D(av5, table5, av17, table17)
+  VERIFY_CONTENT_3D(av6, table6, av18, table18)
+  VERIFY_CONTENT_3D(av7, table7, av19, table19)
+  VERIFY_CONTENT_3D(av8, table8, av20, table20)
+  VERIFY_CONTENT_3D(av9, table9, av21, table21)
+  VERIFY_CONTENT_3D(av10, table10, av22, table22)
+  VERIFY_CONTENT_3D(av11, table11, av23, table23)
+  VERIFY_CONTENT_3D(av12, table12, av24, table24)
+
+  return ret;
+}
+
 int main() {
   bool ret = true;
 
@@ -232,6 +392,10 @@ int main() {
   ret &= test2D<8, 8, 2, 2>();
   ret &= test2D<1024, 16, 32, 4>();
   ret &= test2D<4096, 256, 16, 16>();
+
+  ret &= test3D<1, 1, 1, 1, 1, 1>();
+  ret &= test3D<8, 8, 8, 2, 2, 2>();
+  ret &= test3D<1024, 32, 16, 32, 4, 2>();
 
   return !(ret == true);
 }
