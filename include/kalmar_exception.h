e5fe68e9d2ef88f71bbf0d514371e1b9b506cbf0
Jack Chung <jack@multicorewareinc.com>
2015-07-16 16:14:09 +0800

    [refactor] move exceptions from namespace Concurrency to Kalmar
    - make exceptions under Concurrency namespaces aliases from Kalmar namespace

--- include/kalmar_exception.h
+++ include/kalmar_exception.h
@@ -38,5 +38,24 @@ private:
   HRESULT err_code;
 };
 
+class invalid_compute_domain : public runtime_exception
+{
+public:
+  explicit invalid_compute_domain (const char * message) throw()
+  : runtime_exception(message, E_FAIL) {}
+  invalid_compute_domain() throw()
+  : runtime_exception(E_FAIL) {}
+};
+
+class accelerator_view_removed : public runtime_exception
+{
+public:
+  explicit accelerator_view_removed (const char * message, HRESULT view_removed_reason) throw()
+  : runtime_exception(message, view_removed_reason) {}
+  accelerator_view_removed(HRESULT view_removed_reason) throw()
+  : runtime_exception(view_removed_reason) {}
+  HRESULT get_view_removed_reason() const throw() { return get_error_code(); }
+};
+
 } // namespace Kalmar
 
