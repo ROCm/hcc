7c8b055e03aeb435429e702a31c5d03881205bfc
Jack Chung <jack@multicorewareinc.com>
2015-07-16 15:03:56 +0800

    [refactor] split workitem indexing butilins into a separate header

--- include/amp.h
+++ include/amp.h
@@ -33,6 +33,7 @@
 // CLAMP
 #define __global
 #include <amp_exception.inl>
+#include <kalmar_defines.h>
 #include <kalmar_runtime.h>
 #include <kalmar_buffer.h>
 #include <kalmar_serialize.h>
@@ -44,19 +45,6 @@
 #ifndef WIN32
 #define __declspec(ignored) /* */
 #endif
-
-//
-// work-item related builtin functions
-//
-extern "C" __attribute__((pure)) int64_t amp_get_global_id(unsigned int n) restrict(amp);
-extern "C" __attribute__((pure)) int64_t amp_get_local_id(unsigned int n) restrict(amp);
-extern "C" __attribute__((pure)) int64_t amp_get_group_id(unsigned int n) restrict(amp);
-#if __KALMAR_ACCELERATOR__ == 2 || __KALMAR_CPU__ == 2
-#define tile_static thread_local
-#else
-#define tile_static static __attribute__((section("clamp_opencl_local")))
-#endif
-extern "C" __attribute__((noduplicate)) void amp_barrier(unsigned int n) restrict(amp);
 
 namespace concurrency = Concurrency;
 
