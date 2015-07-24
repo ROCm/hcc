510b03a28c212b0aca4d726a04da4c162fbbacf8
Jack Chung <jack@multicorewareinc.com>
2015-07-24 13:24:50 +0800

    [test] add one test case for __attribute__ ((hc))
    Currently __attribute__ ((hc)) is defined to be just an alias of
    __attribute__ ((amp)) .

---
+++ tests/Unit/Parse/lambda_attribute_hc.cpp
@@ -0,0 +1,66 @@
+// RUN: %cxxamp %s -o %t.out && %t.out
+
+#include <amp.h>
+
+#include <iostream>
+
+#define SIZE (16)
+
+int main() {
+
+  bool ret = true;
+  using namespace concurrency;
+
+  array_view<int, 1> av(SIZE);
+
+  {
+    // case 1: placed between parameter list and left bracket
+    auto k1 = [=] (const index<1>& idx) __attribute__((hc)) {
+      av[idx] = idx[0];
+    };
+  
+    parallel_for_each(extent<1>(SIZE), k1);
+  
+    for (int i = 0; i < SIZE; ++i) {
+      if (av[i] != i) {
+        ret = false;
+        break;
+      }
+    }
+  }
+
+  {
+    // case 2: placed between lambda introducer and parameter list
+    auto k2 = [=] __attribute__((hc)) (const index<1>& idx) {
+      av[idx] = idx[0];
+    };
+  
+    parallel_for_each(extent<1>(SIZE), k2);
+  
+    for (int i = 0; i < SIZE; ++i) {
+      if (av[i] != i) {
+        ret = false;
+        break;
+      }
+    }
+  }
+
+  {
+    // case 3: placed in front of lambda introducer
+    auto k3 = __attribute__((hc)) [=] (const index<1>& idx) {
+      av[idx] = idx[0];
+    };
+  
+    parallel_for_each(extent<1>(SIZE), k3);
+  
+    for (int i = 0; i < SIZE; ++i) {
+      if (av[i] != i) {
+        ret = false;
+        break;
+      }
+    }
+  }
+
+  return !(ret == true);
+}
+
