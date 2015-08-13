cbb85fedf7949d94fbf868f34418cb1d623cb33b
Jack Chung <jack@multicorewareinc.com>
2015-08-13 11:54:47 +0800

    [hc test] add one test case which use accelerator_view::getPendingAsyncOps()
    We want to have a new API which helps check how many async operations are not
    waited in a given accelerator_view instance.  This test case check this feature.

---
+++ tests/Unit/HC/test2.cpp
@@ -0,0 +1,73 @@
+// XFAIL: Linux
+// RUN: %cxxamp %s -Xclang -fhsa-ext -o %t.out && %t.out
+
+#include <hc.hpp>
+
+#include <iostream>
+
+#define TEST_DEBUG (0)
+
+// loop to deliberately slow down kernel execution
+#define LOOP_COUNT (1024)
+
+#define GRID_SIZE (32)
+
+/// test fetching the number of async operations associated with one accelerator_view
+int main() {
+
+  bool ret = true;
+
+  std::vector<int> table1(GRID_SIZE);
+  std::vector<int> table2(GRID_SIZE);
+
+  std::vector<int> table3(GRID_SIZE);
+  std::vector<int> table4(GRID_SIZE);
+  std::vector<int> table5(GRID_SIZE);
+
+  for (int i = 0; i < GRID_SIZE; ++i) {
+    table1[i] = i;
+    table2[i] = i;
+  }
+
+  hc::array_view<const int, 1> av1(GRID_SIZE, table1);
+  hc::array_view<const int, 1> av2(GRID_SIZE, table2);
+  hc::array_view<int, 1> av3(GRID_SIZE, table3);
+  hc::array_view<int, 1> av4(GRID_SIZE, table3);
+  hc::array_view<int, 1> av5(GRID_SIZE, table3);
+
+  hc::accelerator_view accelerator_view = hc::accelerator().get_default_view();
+
+  // do 3 kernel dispatches + 3 barriers
+  hc::parallel_for_each(hc::extent<1>(GRID_SIZE), [=](hc::index<1>& idx) restrict(amp) {
+    for (int i = 0; i < LOOP_COUNT; ++i)
+      av3(idx) = av1(idx) + av2(idx);
+  });
+  
+  accelerator_view.create_marker();
+
+  hc::parallel_for_each(hc::extent<1>(GRID_SIZE), [=](hc::index<1>& idx) restrict(amp) {
+    for (int i = 0; i < LOOP_COUNT; ++i)
+      av4(idx) = av1(idx) + av2(idx);
+  });
+
+  accelerator_view.create_marker();
+
+  hc::parallel_for_each(hc::extent<1>(GRID_SIZE), [=](hc::index<1>& idx) restrict(amp) {
+    for (int i = 0; i < LOOP_COUNT; ++i)
+      av5(idx) = av1(idx) + av2(idx);
+  });
+
+  accelerator_view.create_marker();
+
+  // now there must be 6 pending async operations for the accelerator_view
+  ret &= (accelerator_view.getPendingAsyncOps() == 6);
+
+  // wait for async operations to complete
+  hc::accelerator().get_default_view().wait();
+
+  // now there must be 0 pending async operations for the accelerator_view
+  ret &= (accelerator_view.getPendingAsyncOps() == 0);
+
+  return !(ret == true);
+}
+
