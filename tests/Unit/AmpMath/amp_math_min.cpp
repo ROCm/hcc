b7dcb96c943793bb6e65a6dcf71ebc75d51c1389
Jack Chung <jack@multicorewareinc.com>
2015-09-02 16:23:00 +0800

    [FP test] add 4 new tests for new FP functions
    Per request, add max()/min() as alias of fmax()/fmin().

---
+++ tests/Unit/AmpMath/amp_math_min.cpp
@@ -0,0 +1,44 @@
+// RUN: %cxxamp %s -o %t.out && %t.out
+#include <amp.h>
+#include <stdlib.h>
+#include <iostream>
+#include <amp_math.h>
+
+using namespace concurrency;
+
+int main(void) {
+  const int vecSize = 1000;
+
+  // Alloc & init input data
+  extent<1> e(vecSize);
+  array<float, 1> a(vecSize);
+  array<float, 1> b(vecSize);
+  array<float, 1> c(vecSize);
+  array<float, 1> d(vecSize);
+
+  array_view<float> ga(a);
+  array_view<float> gb(b);
+  array_view<float> gc(c);
+  array_view<float> gd(d);
+
+  for (index<1> i(0); i[0] < vecSize; i++) {
+    ga[i] = rand() / 1000.0f;
+    gb[i] = rand() / 1000.0f;
+  }
+
+  parallel_for_each(
+    e,
+    [=](index<1> idx) restrict(amp) {
+    gc[idx] = fast_math::min(ga[idx], gb[idx]);
+  });
+
+  for(unsigned i = 0; i < vecSize; i++) {
+    gd[i] = fast_math::min(ga[i], gb[i]);
+  }
+
+  float sum = 0;
+  for(unsigned i = 0; i < vecSize; i++) {
+    sum += fast_math::fabs(fast_math::fabs(gc[i]) - fast_math::fabs(gd[i]));
+  }
+  return (sum > 0.1f);
+}
