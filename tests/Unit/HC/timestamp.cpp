dfa04b5c3ec90ee3826785fd5970752222cb7515
Jack Chung <jack@multicorewareinc.com>
2015-09-02 14:32:29 +0800

    [hc test] add one test case to use new timestamp API

---
+++ tests/Unit/HC/timestamp.cpp
@@ -0,0 +1,89 @@
+// XFAIL: Linux
+// RUN: %hc %s -o %t.out && %t.out
+
+#include <hc.hpp>
+
+#include <iostream>
+#include <random>
+
+// loop to deliberately slow down kernel execution
+#define LOOP_COUNT (10244)
+
+#define TEST_DEBUG (0)
+
+// An example which shows how to use accelerator_view::create_marker()
+// and how to use timestamp function
+bool test() {
+  bool ret = true;
+
+  // define inputs and output
+  const int vecSize = 2048;
+
+  int table_a[vecSize];
+  int table_b[vecSize];
+  int table_c[vecSize];
+  int *p_a = &table_a[0];
+  int *p_b = &table_b[0];
+  int *p_c = &table_c[0];
+
+  // initialize test data
+  std::random_device rd;
+  std::uniform_int_distribution<int32_t> int_dist;
+  for (int i = 0; i < vecSize; ++i) {
+    table_a[i] = int_dist(rd);
+    table_b[i] = int_dist(rd);
+  }
+
+  // launch kernel
+  hc::extent<1> e(vecSize);
+  hc::completion_future fut = hc::parallel_for_each(
+    e,
+    [=](hc::index<1> idx) restrict(amp) {
+      for (int i = 0; i < LOOP_COUNT; ++i) 
+        p_c[idx[0]] = p_a[idx[0]] + p_b[idx[0]];
+
+  });
+
+  // create a barrier packet
+  hc::accelerator_view av = hc::accelerator().get_default_view();
+  hc::completion_future fut2 = av.create_marker();
+
+  // fut and fut2 should have the same timestamp frequency
+  ret &= (fut.getTimestampFrequency() > 0);
+  ret &= (fut2.getTimestampFrequency() > 0);
+  ret &= (fut.getTimestampFrequency() == fut2.getTimestampFrequency());
+
+  // wait on the barrier packet
+  fut2.wait();
+
+  // the barrier packet would ensure all previous packets were processed
+
+  // by now the 1st future object should also ended as well
+  ret &= (fut.getEndTimestamp() > fut.getBeginTimestamp());
+
+  // the 2nd future object must have ended, too
+  ret &= (fut2.getEndTimestamp() > fut2.getBeginTimestamp());
+
+  // verify
+  int error = 0;
+  for(unsigned i = 0; i < vecSize; i++) {
+    error += table_c[i] - (table_a[i] + table_b[i]);
+  }
+  if (error == 0) {
+    std::cout << "Verify success!\n";
+  } else {
+    std::cout << "Verify failed!\n";
+  }
+  ret &= (error == 0);
+
+  return ret;
+}
+
+int main() {
+  bool ret = true;
+
+  ret &= test();
+
+  return !(ret == true);
+}
+
