d963f7109c0abb6d82f0ba170387d99a527fc5b4
Kevin Wu <kevin@multicorewareinc.com>
2015-09-03 14:48:18 -0500

    [hip test] Add test to check pfe and direct function calls together
    vectoradd_hc.cpp uses Concurrency pfe in addition to a direct kernel
    function call. Both should be supported by Kalamr.

---
+++ tests/Unit/HIP/vectoradd_hc.cpp
@@ -0,0 +1,95 @@
+// RUN: %hip %s -o %t.out && %t.out
+
+#include <iostream>
+#include <cmath>
+#include <amp.h>
+#include <hip.h>
+
+#define WIDTH     1024
+#define HEIGHT    1024
+
+#define NUM       (WIDTH*HEIGHT)
+
+#define THREADS_PER_BLOCK_X  16
+#define THREADS_PER_BLOCK_Y  16
+
+using namespace Concurrency;
+
+__KERNEL void scalarMulAdd(grid_launch_parm lp, float* out, const float *in, const float scalar, int width, int height)
+{
+
+  GRID_LAUNCH_INIT(lp);
+
+  int x = lp.groupDim.x * lp.groupId.x + lp.threadId.x;
+  int y = lp.groupDim.y * lp.groupId.y + lp.threadId.y;
+
+  int i = y * width + x;
+  if ( i < (width * height)) {
+    out[i] += in[i] * scalar;
+  }
+}
+
+int main() {
+
+  float* A = new float[NUM];
+  float* B = new float[NUM];
+  float* C = new float[NUM];
+
+  float* hostD = (float*)malloc(NUM * sizeof(float));
+  float* deviceD = NULL;
+  HC_ASSERT(hcMalloc((void**)&deviceD, NUM * sizeof(float)));
+
+  // initialize the input data
+  for (int i = 0; i < NUM; i++) {
+    B[i] = (float)i;
+    C[i] = (float)i*100.0f;
+    hostD[i] = (float)i*321.0f;
+  }
+
+  // launch kernel
+  parallel_for_each(
+    extent<2>(WIDTH, HEIGHT).tile<THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y>(),
+    [=](tiled_index<THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y> idx) restrict(amp)
+    {
+      int x = idx.tile_dim0 * idx.tile[0] + idx.local[0];
+      int y = idx.tile_dim1 * idx.tile[1] + idx.local[1];
+
+      int i = y * WIDTH + x;
+      if (i < NUM) {
+        A[i] = B[i] + C[i];
+      }
+  });
+
+  HC_ASSERT(hcMemcpy(deviceD, hostD, NUM*sizeof(float), hcMemcpyHostToAccelerator));
+  const float scalar = 77;
+
+  grid_launch_parm lp = hcCreateLaunchParam2(
+    DIM3(WIDTH/THREADS_PER_BLOCK_X, HEIGHT/THREADS_PER_BLOCK_Y),
+    DIM3(THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y));
+  scalarMulAdd(lp, A, deviceD, scalar, WIDTH, HEIGHT);
+
+  HC_ASSERT(hcMemcpy(hostD, deviceD, NUM*sizeof(float), hcMemcpyAcceleratorToHost));
+
+
+  // verify the results
+  int errors = 0;
+  for (int i = 0; i < NUM; i++) {
+    float expect = B[i] + C[i];
+    expect += hostD[i] * scalar;
+    float actual = A[i];
+    if(std::abs(actual - expect)/((actual + expect)/2) > 0.00001) {
+      errors++;
+    }
+  }
+  if (errors!=0) {
+    std::cout << errors << " errors" << std::endl;
+  }
+
+  delete [] A;
+  delete [] B;
+  delete [] C;
+
+  return errors;
+}
+
+// CHECK:
