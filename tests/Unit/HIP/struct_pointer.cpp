5d27480ebc602f2f72496382cddbd31d8c517c17
Kevin Wu <kevin@multicorewareinc.com>
2015-10-02 03:24:56 -0500

    [gridlaunch test] Test passing in custom type as pointer to kernel

---
+++ tests/Unit/HIP/struct_pointer.cpp
@@ -0,0 +1,43 @@
+// XFAIL: Linux
+// RUN: %hc %s -o %t.out && %t.out
+
+// Test passing a custom type by pointer
+
+#include "hip.h"
+
+#define WIDTH 64
+#define HEIGHT 64
+#define SIZE WIDTH*HEIGHT
+
+#define GRID_SZ 16
+
+__KERNEL void kernel_call(grid_launch_parm lp, float* data1, hcArray* array1)
+{
+  int idx = lp.threadId.x + lp.threadId.y*WIDTH;
+
+  data1[idx] = array1->data[idx];
+
+}
+
+int main()
+{
+
+  hcArray* array1;
+
+  float* data1;
+
+  hcMalloc((void**)&data1, WIDTH*HEIGHT*sizeof(float));
+
+  hcChannelFormatDesc desc = hcCreateChannelDesc();
+
+  hcMallocArray(&array1, &desc, WIDTH, HEIGHT);
+  for(int i = 0; i < SIZE; ++i)
+    array1->data[i] = (float)i;
+
+  dim3 grid = DIM3(WIDTH/GRID_SZ, HEIGHT/GRID_SZ);
+  dim3 block = DIM3(GRID_SZ, GRID_SZ);
+
+  hcLaunchKernel(kernel_call, grid, block, data1, array1);
+
+  return 0;
+}
