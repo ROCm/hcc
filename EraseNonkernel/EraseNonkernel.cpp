8c206fb716e9070d2e550a040a42f5a11e101eb8
Jack Chung <jack@multicorewareinc.com>
2015-10-13 12:20:19 +0800

    [EraseNonkerne] remove unused codes

--- EraseNonkernel/EraseNonkernel.cpp
+++ EraseNonkernel/EraseNonkernel.cpp
@@ -197,9 +197,6 @@ bool EraseNonkernels::runOnModule(Module &M)
                         I->getName().find("get_group_segment_addr") != StringRef::npos || 
                         I->getName().find("get_static_group_segment_size") != StringRef::npos || 
                         I->getName().find("get_dynamic_group_segment_size") != StringRef::npos || 
-                        I->getName().find("get_dynamic_group_segment_cursor") != StringRef::npos || 
-                        I->getName().find("set_dynamic_group_segment_cursor") != StringRef::npos || 
-                        I->getName().find("reset_dynamic_group_segment_cursor") != StringRef::npos || 
                         I->getName().find("_Znwm") != StringRef::npos ||
                         I->getName().find("_Znam") != StringRef::npos ||
                         I->getName().find("_ZdlPv") != StringRef::npos ||
