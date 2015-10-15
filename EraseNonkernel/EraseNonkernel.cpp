5849a0c1e55fb3dcf19d020cedfa9ff82494527c
Jack Chung <jack@multicorewareinc.com>
2015-10-15 15:22:21 +0800

    [EraseNonkernel] fix the pass so get_dynamic_group_segment() get through

--- EraseNonkernel/EraseNonkernel.cpp
+++ EraseNonkernel/EraseNonkernel.cpp
@@ -197,6 +197,7 @@ bool EraseNonkernels::runOnModule(Module &M)
                         I->getName().find("get_group_segment_addr") != StringRef::npos || 
                         I->getName().find("get_static_group_segment_size") != StringRef::npos || 
                         I->getName().find("get_dynamic_group_segment_size") != StringRef::npos || 
+                        I->getName().find("get_dynamic_group_segment") != StringRef::npos || 
                         I->getName().find("_Znwm") != StringRef::npos ||
                         I->getName().find("_Znam") != StringRef::npos ||
                         I->getName().find("_ZdlPv") != StringRef::npos ||
