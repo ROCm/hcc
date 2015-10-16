fbcf6b610fcade62e73c0eff4e36f6154a91f703
Jack Chung <jack@multicorewareinc.com>
2015-10-16 09:46:56 +0800

    [hc test] add one test case for hc::completion_future::is_ready()
    Add one test to check the new API:
    
    hc::compleition_future::is_ready()
    
    which is used to check if the underlying async operations inside a
    completion_future has been completed.

---
+++ tests/Unit/HC/completion_future_is_ready.cpp
@@ -0,0 +1,85 @@
+// XFAIL: Linux
+// RUN: %hc %s -o %t.out && %t.out
+
+#include <hc.hpp>
+
+#include <iostream>
+#include <random>
+
+#include <hsa.h>
+
+// loop to deliberately slow down kernel execution
+#define LOOP_COUNT (1024)
+
+#define TEST_DEBUG (0)
+
+// An example which shows how to use completion_future::is_ready()
+bool test() {
+  bool ret = true;
+
+  // define inputs and output
+  const int vecSize = 2048;
+
+  int table_a[vecSize];
+  int table_b[vecSize];
+  int table_c[vecSize];
+  int *p_a = &table_a[0];
+  int *p_b = &table_b[0];
+  int *p_c = &table_c[0];
+
+  // initialize test data
+  std::random_device rd;
+  std::uniform_int_distribution<int32_t> int_dist;
+  for (int i = 0; i < vecSize; ++i) {
+    table_a[i] = int_dist(rd);
+    table_b[i] = int_dist(rd);
+  }
+
+  // launch kernel
+  hc::extent<1> e(vecSize);
+  hc::completion_future fut = hc::parallel_for_each(
+    e,
+    [=](hc::index<1> idx) restrict(amp) {
+      for (int i = 0; i < LOOP_COUNT; ++i) 
+        p_c[idx[0]] = p_a[idx[0]] + p_b[idx[0]];
+
+  });
+
+  // create a barrier packet
+  hc::accelerator_view av = hc::accelerator().get_default_view();
+  hc::completion_future fut2 = av.create_marker();
+
+  ret &= (fut.is_ready() == false);
+  ret &= (fut2.is_ready() == false);
+
+  // wait on the barrier packet
+  fut2.wait();
+
+  // the barrier packet would ensure all previous packets were processed
+  ret &= (fut.is_ready() == true);
+  ret &= (fut2.is_ready() == true);
+
+  // verify
+  int error = 0;
+  for(unsigned i = 0; i < vecSize; i++) {
+    error += table_c[i] - (table_a[i] + table_b[i]);
+  }
+  if (error == 0) {
+    std::cout << "Verify success!\n";
+  } else {
+    std::cout << "Verify failed!\n";
+  }
+  ret &= (error == 0);
+
+  return ret;
+}
+
+int main() {
+  bool ret = true;
+
+  ret &= test();
+
+  return !(ret == true);
+}
+
+
