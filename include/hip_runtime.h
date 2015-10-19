ecf2b402a88982dfc85b6dae0daa16c033484af8
Yan-Ming Li <yanming@multicorewareinc.com>
2015-10-19 04:21:03 -0500

    [HIP] add asynchronous hipMemcpy/hipMemset API stubs according to cuda APIs

--- include/hip_runtime.h
+++ include/hip_runtime.h
@@ -71,3 +71,108 @@ hipError_t hipGetDeviceCount(int *count) {
   *count = g_device->size();
   return hipSuccess;
 }
+
+
+//-------------------------------
+// hipMemcpy*Async
+//-------------------------------
+
+hipError_t hipMemcpy2DAsync(void *dst, size_t dpitch,
+                            const void *src, size_t spitch,
+                            size_t width, size_t height,
+                            hipMemcpyKind kind,
+                            hipStream_t stream=nullptr) {
+  assert(0 && "hipMemcpy2DAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpy2DFromArrayAsync(void *dst, size_t dpitch,
+                                     const hipArray *src,
+                                     size_t wOffset, size_t hOffset,
+                                     size_t width, size_t height,
+                                     hipMemcpyKind kind,
+                                     hipStream_t stream=nullptr) {
+  assert(0 && "hipMemcpy2DFromArrayAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpy2DToArrayAsync(hipArray *dst,
+                                   size_t wOffset, size_t hOffset,
+                                   const void *src, size_t spitch,
+                                   size_t width, size_t height,
+                                   hipMemcpyKind kind,
+                                   hipStream_t stream=nullptr) {
+  assert(0 && "hipMemcpy2DToArrayAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpyAsync(void *dst, const void *src,
+                          size_t  count,
+                          hipMemcpyKind kind,
+                          hipStream_t stream=nullptr) {
+
+}
+
+
+hipError_t hipMemcpyFromArrayAsync(void *dst, const hipArray *src,
+                                   size_t wOffset, size_t hOffset,
+                                   size_t count,
+                                   hipMemcpyKind kind,
+                                   hipStream_t stream=nullptr) {
+  assert(0 && "hipMemcpyFromArrayAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpyFromSymbolAsync(void *dst, const char *symbol,
+                                    size_t count, size_t offset,
+                                    hipMemcpyKind kind,
+                                    hipStream_t stream=nullptr) {
+  assert(0 && "hipMemcpyFromSymbolAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpyPeerAsync(void *dst, int dstDevice,
+                              const void *src, int srcDevice,
+                              size_t count,
+                              hipStream_t stream =nullptr) {
+  assert(0 && "hipMemcpyPeerAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpyToArrayAsync(hipArray *dst,
+                                 size_t wOffset, size_t hOffset,
+                                 const void *src,
+                                 size_t count,
+                                 hipMemcpyKind kind,
+                                 hipStream_t stream = 0) {
+
+  assert(0 && "hipMemcpyToArrayAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemcpyToSymbolAsync(const char *symbol,
+                                  const void *src,
+                                  size_t count, size_t offset,
+                                  hipMemcpyKind kind,
+                                  hipStream_t stream=nullptr) {
+
+  assert(0 && "hipMemcpyToSymbolAsync unimplemented yet.");
+}
+
+
+//-------------------------------
+// hipMemset*Async
+//-------------------------------
+
+hipError_t hipMemset2DAsync(void *devPtr, size_t pitch, int value,
+                            size_t width, size_t height,
+                            hipStream_t stream=nullptr) {
+
+  assert(0 && "hipMemset2DAsync unimplemented yet.");
+}
+
+
+hipError_t hipMemsetAsync (void *evPtr, int value, size_t count,
+                           hipStream_t stream=nullptr) {
+  assert(0 && "hipMemset unimplemented yet.");
+}
