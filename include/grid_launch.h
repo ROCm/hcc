b966385fedde2dc771ca97aeb5cbbb80cc59f04c
Kevin Wu <kevin@multicorewareinc.com>
2015-11-17 17:36:33 -0600

    [grid_launch] Add init for grid_launch for non-hip use cases

--- include/grid_launch.h
+++ include/grid_launch.h
@@ -32,4 +32,13 @@ typedef struct grid_launch_parm
 #endif
 } grid_launch_parm;
 
+// TODO: Will move to separate source file in the future
+extern inline void grid_launch_init(grid_launch_parm *lp) {
+  lp->gridDim.x = lp->gridDim.y = lp->gridDim.z = 1;
 
+  lp->groupDim.x = lp->groupDim.y = lp->groupDim.z = 1;
+
+  lp->groupMemBytes = 0;
+  static hc::accelerator_view av = hc::accelerator().get_default_view();
+  lp->av = &av;
+}
