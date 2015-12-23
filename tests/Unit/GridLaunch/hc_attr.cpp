b26646494f1ea6fb0320daeb09145280170a902c
Kevin Wu <kevin@multicorewareinc.com>
2015-12-23 14:16:32 -0600

    [grid_launch test] Test for calling [[hc]] atttribute functions from gl kernel
    Also test for __attribute__((hc)) which should have the same behavior

---
+++ tests/Unit/GridLaunch/hc_attr.cpp
@@ -0,0 +1,64 @@
+// RUN: %hc -lhc_am %s -o %t.out && %t.out
+
+#include "hc.hpp"
+#include "grid_launch.h"
+#include "hc_am.hpp"
+#include <iostream>
+
+#define GRID_SIZE 16
+#define TILE_SIZE 16
+
+// C++11 style attribute
+[[hc]] int foo(grid_launch_parm lp) {
+  int idx = lp.threadId.x + lp.groupId.x*lp.groupDim.x;
+  return idx;
+}
+
+// C-style attribute
+__attribute__((hc)) int foo2(grid_launch_parm lp) {
+  int idx = lp.threadId.x + lp.groupId.x*lp.groupDim.x;
+  return idx + 2;
+}
+
+// No attribute
+int foo3(grid_launch_parm lp) {
+  int idx = lp.threadId.x + lp.groupId.x*lp.groupDim.x;
+  return idx + 3;
+}
+
+__attribute__((hc_grid_launch)) void kernel(grid_launch_parm lp, int* x) {
+  int idx = foo(lp);
+  x[idx] = idx + foo2(lp) + foo3(lp);
+}
+
+
+int main() {
+
+  const int sz = 10;
+
+  int* data1 = (int* )malloc(sz*sizeof(int));
+
+  int* data1_d = (int*)hc::am_alloc(sz*sizeof(int), hc::accelerator(), 0);
+
+  grid_launch_parm lp;
+  grid_launch_init(&lp);
+
+  lp.gridDim = uint3(GRID_SIZE, 1);
+  lp.groupDim = uint3(TILE_SIZE, 1);
+
+  hc::completion_future cf;
+  lp.cf = &cf;
+  kernel(lp, data1_d);
+  lp.cf->wait();
+
+  hc::am_copy(data1, data1_d, sz*sizeof(int));
+
+  bool ret = true;
+
+  for(int i = 0; i < sz; ++i) {
+    ret &= (data1[i] == i + i + 2 + i + 3);
+  }
+
+  return !ret;
+
+}
