e285be910e6b704e3041eb947b2347e1e5554ed0
Jack Chung <jack@multicorewareinc.com>
2016-01-05 16:00:54 +0800

    [HSAIL test] revise test for __shfl_down to check float type as well

--- tests/Unit/HSAIL/shfl_down.cpp
+++ tests/Unit/HSAIL/shfl_down.cpp
@@ -15,24 +15,25 @@
 // - __shfl_down
 
 // test __shfl_down
-bool test__shfl_down(int grid_size, int arg) {
+template<typename T>
+bool test__shfl_down(int grid_size, int offset, T init_value) {
   bool ret = true;
 
   using namespace hc;
   extent<1> ex(grid_size);
-  array<int, 1> table(grid_size);
+  array<T, 1> table(grid_size);
 
   // shift values down in a wavefront
-  parallel_for_each(ex, [&, arg](index<1>& idx) [[hc]] {
-    int value = hsail_activelaneid_u32();
-    value = __shfl_down(value, arg);
+  parallel_for_each(ex, [&, offset, init_value](index<1>& idx) [[hc]] {
+    T value = init_value + hsail_activelaneid_u32();
+    value = __shfl_down(value, offset);
     table(idx) = value;
   }).wait();
 
-  std::vector<int> output = table;
+  std::vector<T> output = table;
   for (int i = 0; i < grid_size; ++i) {
     int laneId = i % WAVEFRONT_SIZE;
-    ret &= (output[i] == (((laneId + arg) >= WAVEFRONT_SIZE) ? laneId : laneId + arg));
+    ret &= (output[i] == (init_value + (((laneId + offset) >= WAVEFRONT_SIZE) ? laneId : laneId + offset)));
 #if TEST_DEBUG
     std::cout << output[i] << " ";
     if ((i + 1) % WAVEFRONT_SIZE == 0)
@@ -47,24 +48,25 @@ bool test__shfl_down(int grid_size, int arg) {
 }
 
 // test __shfl_down with different sub-wavefront widths
-bool test__shfl_down2(int grid_size, int sub_wavefront_width, int arg) {
+template<typename T>
+bool test__shfl_down2(int grid_size, int sub_wavefront_width, int offset, T init_value) {
   bool ret = true;
 
   using namespace hc;
   extent<1> ex(grid_size);
-  array<int, 1> table(grid_size);
+  array<T, 1> table(grid_size);
 
   // shift values down in a wavefront, divided into subsections
-  parallel_for_each(ex, [&, arg, sub_wavefront_width](index<1>& idx) [[hc]] {
-    int value = hsail_activelaneid_u32() % sub_wavefront_width;
-    value = __shfl_down(value, arg, sub_wavefront_width);
+  parallel_for_each(ex, [&, offset, sub_wavefront_width, init_value](index<1>& idx) [[hc]] {
+    T value = init_value + (hsail_activelaneid_u32() % sub_wavefront_width);
+    value = __shfl_down(value, offset, sub_wavefront_width);
     table(idx) = value;
   }).wait();
 
-  std::vector<int> output = table;
+  std::vector<T> output = table;
   for (int i = 0; i < grid_size; ++i) {
     int laneId = i % sub_wavefront_width;
-    ret &= (output[i] == (((laneId + arg) >= sub_wavefront_width) ? laneId : laneId + arg));
+    ret &= (output[i] == (init_value + (((laneId + offset) >= sub_wavefront_width) ? laneId : laneId + offset)));
 #if TEST_DEBUG
     std::cout << output[i] << " ";
     if ((i + 1) % WAVEFRONT_SIZE == 0)
@@ -84,72 +86,141 @@ int main() {
   // NOTICE: the test case is designed so grid size is always a multiple of wavefront size (64),
   // so there will not be any inactive work-items
 
+  std::random_device rd;
+  std::uniform_int_distribution<int> int_dist(0, 1023);
+  std::uniform_real_distribution<float> float_dist(0.0, 1.0);
+
   // test __shfl_down for different grid sizes and offsets
-  ret &= test__shfl_down(64, 1);
-  ret &= test__shfl_down(64, 2);
-  ret &= test__shfl_down(64, 4);
-  ret &= test__shfl_down(64, 8);
-  ret &= test__shfl_down(64, 15);
-  ret &= test__shfl_down(128, 1);
-  ret &= test__shfl_down(128, 2);
-  ret &= test__shfl_down(128, 4);
-  ret &= test__shfl_down(128, 8);
-  ret &= test__shfl_down(128, 16);
-  ret &= test__shfl_down(128, 32);
-  ret &= test__shfl_down(1024, 1);
-  ret &= test__shfl_down(1024, 2);
-  ret &= test__shfl_down(1024, 4);
-  ret &= test__shfl_down(1024, 8);
-  ret &= test__shfl_down(1024, 16);
-  ret &= test__shfl_down(1024, 32);
+  ret &= test__shfl_down<int>(64, 1, int_dist(rd));
+  ret &= test__shfl_down<int>(64, 2, int_dist(rd));
+  ret &= test__shfl_down<int>(64, 4, int_dist(rd));
+  ret &= test__shfl_down<int>(64, 8, int_dist(rd));
+  ret &= test__shfl_down<int>(64, 15, int_dist(rd));
+  ret &= test__shfl_down<int>(128, 1, int_dist(rd));
+  ret &= test__shfl_down<int>(128, 2, int_dist(rd));
+  ret &= test__shfl_down<int>(128, 4, int_dist(rd));
+  ret &= test__shfl_down<int>(128, 8, int_dist(rd));
+  ret &= test__shfl_down<int>(128, 16, int_dist(rd));
+  ret &= test__shfl_down<int>(128, 32, int_dist(rd));
+  ret &= test__shfl_down<int>(1024, 1, int_dist(rd));
+  ret &= test__shfl_down<int>(1024, 2, int_dist(rd));
+  ret &= test__shfl_down<int>(1024, 4, int_dist(rd));
+  ret &= test__shfl_down<int>(1024, 8, int_dist(rd));
+  ret &= test__shfl_down<int>(1024, 16, int_dist(rd));
+  ret &= test__shfl_down<int>(1024, 32, int_dist(rd));
+
+  ret &= test__shfl_down<float>(64, 1, float_dist(rd));
+  ret &= test__shfl_down<float>(64, 2, float_dist(rd));
+  ret &= test__shfl_down<float>(64, 4, float_dist(rd));
+  ret &= test__shfl_down<float>(64, 8, float_dist(rd));
+  ret &= test__shfl_down<float>(64, 15, float_dist(rd));
+  ret &= test__shfl_down<float>(128, 1, float_dist(rd));
+  ret &= test__shfl_down<float>(128, 2, float_dist(rd));
+  ret &= test__shfl_down<float>(128, 4, float_dist(rd));
+  ret &= test__shfl_down<float>(128, 8, float_dist(rd));
+  ret &= test__shfl_down<float>(128, 16, float_dist(rd));
+  ret &= test__shfl_down<float>(128, 32, float_dist(rd));
+  ret &= test__shfl_down<float>(1024, 1, float_dist(rd));
+  ret &= test__shfl_down<float>(1024, 2, float_dist(rd));
+  ret &= test__shfl_down<float>(1024, 4, float_dist(rd));
+  ret &= test__shfl_down<float>(1024, 8, float_dist(rd));
+  ret &= test__shfl_down<float>(1024, 16, float_dist(rd));
+  ret &= test__shfl_down<float>(1024, 32, float_dist(rd));
 
   // test __shfl_down for different grid sizes, different subsection sizes, and different offsets
-  ret &= test__shfl_down2(64, 2, 1);
-  ret &= test__shfl_down2(64, 4, 1);
-  ret &= test__shfl_down2(64, 4, 2);
-  ret &= test__shfl_down2(64, 8, 1);
-  ret &= test__shfl_down2(64, 8, 3);
-  ret &= test__shfl_down2(64, 8, 5);
-  ret &= test__shfl_down2(64, 16, 1);
-  ret &= test__shfl_down2(64, 16, 2);
-  ret &= test__shfl_down2(64, 16, 4);
-  ret &= test__shfl_down2(64, 16, 8);
-  ret &= test__shfl_down2(64, 32, 1);
-  ret &= test__shfl_down2(64, 32, 2);
-  ret &= test__shfl_down2(64, 32, 3);
-  ret &= test__shfl_down2(64, 32, 4);
-  ret &= test__shfl_down2(64, 32, 7);
-  ret &= test__shfl_down2(64, 32, 15);
-  ret &= test__shfl_down2(128, 2, 1);
-  ret &= test__shfl_down2(128, 4, 1);
-  ret &= test__shfl_down2(128, 4, 2);
-  ret &= test__shfl_down2(128, 8, 1);
-  ret &= test__shfl_down2(128, 8, 2);
-  ret &= test__shfl_down2(128, 8, 4);
-  ret &= test__shfl_down2(128, 16, 1);
-  ret &= test__shfl_down2(128, 16, 2);
-  ret &= test__shfl_down2(128, 16, 4);
-  ret &= test__shfl_down2(128, 16, 8);
-  ret &= test__shfl_down2(128, 32, 1);
-  ret &= test__shfl_down2(128, 32, 2);
-  ret &= test__shfl_down2(128, 32, 4);
-  ret &= test__shfl_down2(128, 32, 8);
-  ret &= test__shfl_down2(128, 32, 16);
-  ret &= test__shfl_down2(1024, 2, 1);
-  ret &= test__shfl_down2(1024, 4, 1);
-  ret &= test__shfl_down2(1024, 4, 2);
-  ret &= test__shfl_down2(1024, 8, 1);
-  ret &= test__shfl_down2(1024, 8, 2);
-  ret &= test__shfl_down2(1024, 8, 4);
-  ret &= test__shfl_down2(1024, 16, 1);
-  ret &= test__shfl_down2(1024, 16, 2);
-  ret &= test__shfl_down2(1024, 16, 4);
-  ret &= test__shfl_down2(1024, 16, 8);
-  ret &= test__shfl_down2(1024, 32, 1);
-  ret &= test__shfl_down2(1024, 32, 2);
-  ret &= test__shfl_down2(1024, 32, 4);
-  ret &= test__shfl_down2(1024, 32, 8);
-  ret &= test__shfl_down2(1024, 32, 16);
+  ret &= test__shfl_down2<int>(64, 2, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 4, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 4, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 8, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 8, 3, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 8, 5, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 16, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 16, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 16, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 16, 8, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 32, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 32, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 32, 3, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 32, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 32, 7, int_dist(rd));
+  ret &= test__shfl_down2<int>(64, 32, 15, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 2, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 4, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 4, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 8, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 8, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 8, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 16, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 16, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 16, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 16, 8, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 32, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 32, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 32, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 32, 8, int_dist(rd));
+  ret &= test__shfl_down2<int>(128, 32, 16, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 2, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 4, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 4, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 8, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 8, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 8, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 16, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 16, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 16, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 16, 8, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 32, 1, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 32, 2, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 32, 4, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 32, 8, int_dist(rd));
+  ret &= test__shfl_down2<int>(1024, 32, 16, int_dist(rd));
+
+  ret &= test__shfl_down2<float>(64, 2, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 4, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 4, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 8, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 8, 3, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 8, 5, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 16, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 16, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 16, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 16, 8, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 32, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 32, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 32, 3, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 32, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 32, 7, float_dist(rd));
+  ret &= test__shfl_down2<float>(64, 32, 15, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 2, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 4, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 4, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 8, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 8, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 8, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 16, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 16, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 16, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 16, 8, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 32, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 32, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 32, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 32, 8, float_dist(rd));
+  ret &= test__shfl_down2<float>(128, 32, 16, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 2, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 4, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 4, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 8, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 8, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 8, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 16, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 16, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 16, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 16, 8, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 32, 1, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 32, 2, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 32, 4, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 32, 8, float_dist(rd));
+  ret &= test__shfl_down2<float>(1024, 32, 16, float_dist(rd));
 
   return !(ret == true);
 }
