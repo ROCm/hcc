d29b8ec475d5d84f30191d2b591f7f00d2d97558
Jack Chung <jack@multicorewareinc.com>
2016-01-05 14:02:32 +0800

    [HSAIL test] revise test for __shfl_up
    implement a scan algorithm based on __shfl_up

--- tests/Unit/HSAIL/shfl_up.cpp
+++ tests/Unit/HSAIL/shfl_up.cpp
@@ -74,6 +74,48 @@ bool test__shfl_up2(int grid_size, int sub_wavefront_width, int arg) {
   return ret;
 }
 
+bool test_scan(int grid_size, int sub_wavefront_width) {
+  bool ret = true;
+
+  using namespace hc;
+  extent<1> ex(grid_size);
+  array<int, 1> table(grid_size);
+
+  parallel_for_each(ex, [&, sub_wavefront_width](index<1>& idx) [[hc]] {
+    int laneId = hsail_activelaneid_u32();
+    int logicalLaneId = laneId % sub_wavefront_width;
+    int value = (WAVEFRONT_SIZE - 1) - laneId;
+
+    for (int i = 1; i <= (sub_wavefront_width / 2); i *= 2) {
+      int n = __shfl_up(value, i, sub_wavefront_width);
+      if (logicalLaneId >= i)
+        value += n;
+    }
+    table(idx) = value;
+  }).wait();
+
+  std::vector<int> output = table;
+  for (int i = 0; i < grid_size; ++i) {
+    int laneId = i % WAVEFRONT_SIZE;
+    int logicalLaneId = laneId % sub_wavefront_width;
+    int subWavefrontId = laneId / sub_wavefront_width;
+    int expected = 0;
+    for (int j = 0; j <= logicalLaneId; ++j) {
+      expected += (WAVEFRONT_SIZE - 1) - (sub_wavefront_width * subWavefrontId) - j;
+    }
+    ret &= (output[i] == expected);
+#if TEST_DEBUG
+    //std::cout << expected << " ";
+    std::cout << output[i] << " ";
+#endif
+  }
+#if TEST_DEBUG
+    std::cout << "\n";
+#endif
+
+  return ret;
+}
+
 int main() {
   bool ret = true;
 
@@ -175,6 +217,18 @@ int main() {
   ret &= test__shfl_up2(1023, 32, 4);
   ret &= test__shfl_up2(1023, 32, 8);
   ret &= test__shfl_up2(1023, 32, 16);
+
+  // test scan algorithm using __shfl_up
+  ret &= test_scan(8, 2);
+  ret &= test_scan(8, 4);
+  ret &= test_scan(32, 8);
+  ret &= test_scan(64, 8);
+  ret &= test_scan(64, 32);
+  ret &= test_scan(64, 64);
+  ret &= test_scan(128, 8);
+  ret &= test_scan(128, 16);
+  ret &= test_scan(128, 32);
+  ret &= test_scan(128, 64);
 
   return !(ret == true);
 }
