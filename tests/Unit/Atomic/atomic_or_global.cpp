a68fad8e10f2ad72dc38958bf70897649b43d6de
Jack Chung <jack@multicorewareinc.com>
2016-01-07 17:41:48 +0800

    [test] add 6 test cases for atomic_fetch_and, atomic_fetch_or, atomic_fetch_xor

---
+++ tests/Unit/Atomic/atomic_or_global.cpp
@@ -0,0 +1,40 @@
+// RUN: %cxxamp %s -o %t.out && %t.out
+#include <amp.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace concurrency;
+
+int main(void) {
+  const int vecSize = 100;
+
+  // Alloc & init input data
+  int init[vecSize] { 0 };
+  for (int i = 0; i < vecSize; ++i) {
+    init[i] = i;
+  }
+  array<int, 1> count(vecSize, std::begin(init));
+
+  parallel_for_each(count.get_extent(), [=, &count](index<1> idx) restrict(amp) {
+    for(int i = 0; i < vecSize; i++) {
+      atomic_fetch_or(&count[i], 1);
+    }
+  });
+
+  array_view<int, 1> av(count);
+
+  bool ret = true;
+  for(int i = 0; i < vecSize; ++i) {
+    if ( (i % 2) == 0 ) {
+      if (av[i] != (i + 1)) {
+        ret = false;
+      }
+    } else {
+      if (av[i] != i) {
+        ret = false;
+      }
+    }
+  }
+
+  return !(ret == true);
+}
