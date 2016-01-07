3ba82bae3341d5afaff4904e00656d155b0113e9
Jack Chung <jack@multicorewareinc.com>
2016-01-07 14:48:46 +0800

    [test] add 2 test cases for atomic_fetch_dec

---
+++ tests/Unit/Atomic/atomic_dec_global.cpp
@@ -0,0 +1,31 @@
+// RUN: %cxxamp %s -o %t.out && %t.out
+#include <amp.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace concurrency;
+
+int main(void) {
+  const int vecSize = 100;
+
+  // Alloc & init input data
+  int init[vecSize] { 0 };
+  array<int, 1> count(vecSize, std::begin(init));
+
+  parallel_for_each(count.get_extent(), [=, &count](index<1> idx) restrict(amp) {
+    for(unsigned i = 0; i < vecSize; i++) {
+      atomic_fetch_dec(&count[i]);
+    }
+  });
+
+  array_view<int, 1> av(count);
+
+  bool ret = true;
+  for(unsigned i = 0; i < vecSize; ++i) {
+      if(av[i] != -vecSize) {
+        ret = false;
+      }
+  }
+
+  return !(ret == true);
+}
