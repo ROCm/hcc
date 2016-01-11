ba53a113eb390820b68e8de3c5aedf730a98493d
Jack Chung <jack@multicorewareinc.com>
2016-01-11 15:35:10 +0800

    [short vector] initial commit to add short vector support in HC
    - implementation of short vectros have been moved to an inl file
    - amp_short_vectors.h and hc_short_vectors.hpp wraps the inl file in
      corresponding namespaces
    - cmake file changes

---
+++ include/hc_short_vectors.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef _HC_SHORT_VECTORS_HPP
+#define _HC_SHORT_VECTORS_HPP
+
+namespace hc
+{
+
+#include "kalmar_short_vectors.inl"
+
+} // namespace hc
+
+#endif // _HC_SHORT_VECTORS_H
