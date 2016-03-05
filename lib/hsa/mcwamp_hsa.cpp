e00ed05dd78decc527f6e5a1768f6e66afbf7f61
Tom Stellard <thomas.stellard@amd.com>
2016-03-04 19:35:34 -0500

    hsa: Don't set hcc_{static,dynamic)_group_segment_size values when using AMDGPU
    AMDGPU won't use these values, and it adds unnecessary complexity to the
    code object.

--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -2256,11 +2256,13 @@ HSADispatch::dispatchKernel(hsa_queue_t* commandQueue) {
                                             &group_segment_size);
     STATUS_CHECK_Q(status, commandQueue, __LINE__);
 
+#ifndef HSA_USE_AMDGPU_BACKEND
     // let kernel know static group segment size
     kernel->executable->setSymbolToValue("&hcc_static_group_segment_size", group_segment_size);
 
     // let kernel know dynamic group segment size
     kernel->executable->setSymbolToValue("&hcc_dynamic_group_segment_size", this->dynamicGroupSize);
+#endif
 
     // add dynamic group segment size
     group_segment_size += this->dynamicGroupSize;
