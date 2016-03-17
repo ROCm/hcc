fe25ffd1bcc561f25b15f689afec885564d3fa76
Ben Sander <ben.sander@amd.com>
2016-03-17 00:33:09 -0500

    Add staging buffer class files to HCC make infrastructure.

---
+++ include/staging_buffer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "hsa.h"
+
+
+//-------------------------------------------------------------------------------------------------
+struct StagingBuffer {
+
+    static const int _max_buffers = 4;
+
+    StagingBuffer(hc::accelerator &acc, size_t bufferSize, int numBuffers) ;
+    ~StagingBuffer();
+
+    void CopyHostToDevice(void* dst, const void* src, size_t sizeBytes, hsa_signal_t *waitFor);
+    void CopyHostToDevicePinInPlace(void* dst, const void* src, size_t sizeBytes, hsa_signal_t *waitFor);
+
+    void CopyDeviceToHost   (void* dst, const void* src, size_t sizeBytes, hsa_signal_t *waitFor);
+    void CopyDeviceToHostPinInPlace(void* dst, const void* src, size_t sizeBytes, hsa_signal_t *waitFor);
+
+
+private:
+    hc::accelerator &_acc;
+    hsa_agent_t     _hsa_agent;
+    size_t          _bufferSize;  // Size of the buffers.
+    int             _numBuffers;
+
+    char            *_pinnedStagingBuffer[_max_buffers];
+    hsa_signal_t     _completion_signal[_max_buffers];
+};
