27069d19c5fa182267ab7ef420761623307233cc
Yan-Ming Li <yanming@multicorewareinc.com>
2016-03-21 18:37:44 -0500

    Customize serializer for grid_launch_parm struct
    This should improve performance by skipping the serialization of unused members,
    keeping only gridDim, groupDim, threadId, groupId, and groupMemBytes.

--- include/grid_launch.h
+++ include/grid_launch.h
@@ -20,6 +20,48 @@ typedef struct grid_launch_parm
   // use acc_view for PFE in WrapperGen
   hc::accelerator_view  *av;
   hc::completion_future *cf;
+
+  grid_launch_parm() = default;
+
+  // customized serialization: don't need av and cf in kernel
+  __attribute__((annotate("serialize")))
+  void __cxxamp_serialize(Kalmar::Serialize& s) const {
+    s.Append(sizeof(int), &gridDim.x);
+    s.Append(sizeof(int), &gridDim.y);
+    s.Append(sizeof(int), &gridDim.z);
+    s.Append(sizeof(int), &groupDim.x);
+    s.Append(sizeof(int), &groupDim.y);
+    s.Append(sizeof(int), &groupDim.z);
+    s.Append(sizeof(int), &groupId.x);
+    s.Append(sizeof(int), &groupId.y);
+    s.Append(sizeof(int), &groupId.z);
+    s.Append(sizeof(int), &threadId.x);
+    s.Append(sizeof(int), &threadId.y);
+    s.Append(sizeof(int), &threadId.z);
+    s.Append(sizeof(unsigned), &groupMemBytes);
+  }
+
+  __attribute__((annotate("user_deserialize")))
+  grid_launch_parm(int gridDim_x,  int gridDim_y,  int gridDim_z,
+                   int groupDim_x, int groupDim_y, int groupDim_z,
+                   int groupId_x,  int groupId_y,  int groupId_z,
+                   int threadId_x, int threadId_y, int threadId_z,
+                   unsigned groupMemBytes_) {
+    gridDim.x  = gridDim_x;
+    gridDim.y  = gridDim_y;
+    gridDim.z  = gridDim_z;
+    groupDim.x = groupDim_x;
+    groupDim.y = groupDim_y;
+    groupDim.z = groupDim_z;
+    groupId.x  = groupId_x;
+    groupId.y  = groupId_y;
+    groupId.z  = groupId_z;
+    threadId.x = threadId_x;
+    threadId.y = threadId_y;
+    threadId.z = threadId_z;
+    groupMemBytes = groupMemBytes_;
+  }
+
 } grid_launch_parm;
 
 // TODO: Will move to separate source file in the future
