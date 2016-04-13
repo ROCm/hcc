15a631bbadbcfbfad1174d465035ffae6b4ec0b6
scchan <siuchi.chan@amd.com>
2016-04-12 23:09:05 -0500

    don't link to libhsakmt

--- scripts/cmake/MCWAMP.cmake
+++ scripts/cmake/MCWAMP.cmake
@@ -66,7 +66,6 @@ macro(add_mcwamp_library_hsa name )
   add_dependencies(${name} llvm-link opt clang)
   # add HSA libraries
   target_link_libraries(${name} ${HSA_LIBRARY})
-  target_link_libraries(${name} ${HSA_KMT_LIBRARY})
   target_link_libraries(${name} pthread)
   target_link_libraries(${name} elf)
 endmacro(add_mcwamp_library_hsa name )
