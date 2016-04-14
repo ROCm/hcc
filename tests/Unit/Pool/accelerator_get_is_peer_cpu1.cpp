dd904b94f0f29c92bd03bf310a76b7a0aa6c7621
Fan Cao <fan.cao2@amd.com>
2016-04-14 13:00:03 -0400

    Add two more Unit tests for get_is_peer(), and add lhc_am link flag to map_to_peer tests

---
+++ tests/Unit/Pool/accelerator_get_is_peer_cpu1.cpp
@@ -0,0 +1,40 @@
+//XFAIL: Linux
+// RUN: %hc %s -o %t.out && %t.out
+
+#include <hc.hpp>
+
+/**
+ * So far, CPU accelerator is not peer of any other
+ * accelerator. This test will pass each accelerator 
+ * in the system to CPU accelerator and check if 
+ * get_is_peer() will return false.
+ */
+
+int main()
+{
+    // Get CPU accelerator.
+    hc::accelerator cpu;
+
+    const auto& all = hc::accelerator::get_all();
+
+    for(auto iter = all.begin(); iter != all.end(); iter++)
+    {
+        if(iter->get_is_emulated())
+        {
+            cpu = *iter;
+            break;
+        }
+    }
+
+    // Check get_is_peer() return value, if it is true,
+    // then, test fails, return -1.
+    for(auto iter = all.begin(); iter != all.end(); iter++)
+    {
+        if(cpu.get_is_peer(*iter))
+        {
+            return -1;
+        }
+    }
+
+    return 0;
+}
--- tests/Unit/Pool/map_to_peers_host_ptr.cpp
+++ tests/Unit/Pool/map_to_peers_host_ptr.cpp
@@ -1,5 +1,5 @@
 // XFAIL: Linux
-// RUN: %hc %s -o %t.out && %t.out
+// RUN: %hc %s lhc_am -o %t.out && %t.out
 
 #include <hc_am.hpp>
 #include <hc.hpp>
