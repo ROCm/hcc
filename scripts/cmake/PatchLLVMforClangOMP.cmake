daa08b03f678fba9e55f0117169b5a370db1019b
Jack Chung <whchung@gmail.com>
2016-05-23 16:48:15 +0800

    [OpenMP] remove OpenMP tests from HCC
    OpenMP support would be in upstream LLVM / Clang so these codes are no longer
    required in HCC.

--- scripts/cmake/PatchLLVMforClangOMP.cmake
+++
@@ -1,7 +0,0 @@
-macro(patch_LLVM_for_ClangOMP name)
-
-MESSAGE("Sync LLVM with that used by clang-omp.")
-execute_process( COMMAND sh ${PROJECT_SOURCE_DIR}/${name}/LLVM.patch
-                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-
-endmacro()
