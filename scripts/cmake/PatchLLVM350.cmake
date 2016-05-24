84c9d280c09ca7db8e39a40154de8a03b7b409f0
Jack Chung <whchung@gmail.com>
2016-05-24 12:27:39 +0800

    [LLVM] Remove patch for LLVM 3.5 to be built by GCC 5
    The code is already in upstream LLVM.

--- scripts/cmake/PatchLLVM350.cmake
+++
@@ -1,7 +0,0 @@
-macro(patch_LLVM350 name)
-
-MESSAGE("Patch LLVM3.5.0 so it can be compiled with gcc 5")
-execute_process( COMMAND sh ${PROJECT_SOURCE_DIR}/${name}/LLVM350.patch
-                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-
-endmacro()
