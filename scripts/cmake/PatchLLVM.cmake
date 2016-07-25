5a9b27cda445f921bc9b09c1cd09da36a7e5616d
Jack Chung <whchung@gmail.com>
2016-07-25 10:35:58 +0800

    [CMake] add patch for address space in debug metadata

--- scripts/cmake/PatchLLVM.cmake
+++ scripts/cmake/PatchLLVM.cmake
@@ -5,4 +5,9 @@ MESSAGE("Patch LLVM so it has HCC EnvironmentType in Triple.")
 execute_process( COMMAND git apply ${PROJECT_SOURCE_DIR}/${name}/LLVM.patch
                  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/compiler)
 
+MESSAGE("Patch LLVM to support debugging metadata with address space information.")
+execute_process( COMMAND git apply --whitespace=nowarn ${PROJECT_SOURCE_DIR}/${name}/Debugger.patch
+                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/compiler)
+
+
 endmacro()
