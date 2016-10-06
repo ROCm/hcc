5e964eee7015c06bbf7d1b117f345af4913f23d7
Wen-Heng (Jack) Chung <whchung@gmail.com>
2016-10-06 17:39:04 +0800

    [test] fix one test case to match the new error message output

--- tests/Unit/AutoRestricted/Negative/auto-on-wrong-place/before_function_type.cpp
+++ tests/Unit/AutoRestricted/Negative/auto-on-wrong-place/before_function_type.cpp
@@ -14,7 +14,6 @@ restrict(auto) int f_before_function_type() restrict(amp) {return 1;}
 // CHECK-NEXT:         ^~~~
 // CHECK-NEXT:before_function_type.cpp:[[@LINE-4]]:1: error: C++ requires a type specifier for all declarations
 // CHECK-NEXT:restrict(auto) int f_before_function_type() restrict(amp) {return 1;}
-// CHECK-NEXT:^~~~~~~~
 
 int main(void)
 {
