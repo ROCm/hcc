4330d76a6afcc37eb0008151336a55ca5fb714cd
scchan <siuchi.chan@amd.com>
2017-02-06 18:18:01 -0500

    update comment regarding the hash cutoff size

--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -148,7 +148,7 @@ if (COMPILE_HCC_DB && (HCC_DB & (1<<(db_flag)))) { \
 #define FORCE_SIGNAL_DEP_BETWEEN_COPIES (0)
 
 // cutoff size used in FNV-1a hash function
-// default set as 768, this is a heuristic value
+// default set as 2048, this is a heuristic value
 // which is larger than HSA BrigModuleHeader and AMD GCN ISA header (Elf64_Ehdr)
 #define FNV1A_CUTOFF_SIZE (2048)
 
