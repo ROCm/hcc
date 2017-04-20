57dae959b98683d7fda058a65845cab570964741
Siu Chi Chan <siuchi.chan@amd.com>
2017-04-20 13:57:03 -0400

    update printf example
    Change-Id: I62ca785edb2cb204e61a448e7e56a116d19c8b6c

--- tests/Unit/HSA/printf.cpp
+++ tests/Unit/HSA/printf.cpp
@@ -6,8 +6,10 @@
 
 #include <iostream>
 
-#define SIZE (32)
-#define PRINTF_BUFFER_SIZE (512)
+#define TILE (64)
+#define GLOBAL (TILE*2)
+
+#define PRINTF_BUFFER_SIZE (2048)
 
 int main() {
   using namespace hc;
@@ -19,27 +21,9 @@ int main() {
   const char* str2 = "thread";
   const char* str3 = "Hello again from %s: %d\n";
 
-  parallel_for_each(extent<1>(SIZE), [=](index<1> idx) restrict(amp) {
-
-    if (idx[0] == 0) {
-      printf(printf_buf, str1, str2, idx[0]);
-      printf(printf_buf, str3, str2, idx[0]);
-    }
-
-#if 0
-    /* TBD.
-       On dGPU, device addresses of strings on device global memory
-       need to be fetched by hcc runtime somehow */
-    } else if (idx[0] == 5) {
-      printf(printf_buf, s1, "work-item", idx[0]);
-    } else if (idx[0] == 10) {
-      printf(printf_buf, s1, "work item", idx[0]);
-    } else if (idx[0] == 15) {
-      printf(printf_buf, s1, "workitem", idx[0]);
-      printf(printf_buf, "Hello again from %s: %d\n", "thread", idx[0]);
-    }
-#endif
-
+  parallel_for_each(extent<1>(GLOBAL).tile(TILE), [=](tiled_index<1> tidx) [[hc]] {
+      printf(printf_buf, str1, str2, tidx.global[0]);
+      printf(printf_buf, str3, str2, tidx.global[0]);
   }).wait();
 
   processPrintfBuffer(printf_buf);
