c9b26d4556202faec1e82ea6e8be6d5808136af1
Siu Chi Chan <siuchi.chan@amd.com>
2017-04-21 01:54:02 -0400

    fix printf test verification
    Change-Id: Id952c514085ac8d6b4e5afa2fb4804ecdfae5892

--- tests/Unit/HSA/printf.cpp
+++ tests/Unit/HSA/printf.cpp
@@ -17,9 +17,9 @@ int main() {
   accelerator acc = accelerator();
   PrintfPacket* printf_buf = createPrintfBuffer(acc, PRINTF_BUFFER_SIZE);
 
-  const char* str1 = "Hello HC from %s: %d\n";
+  const char* str1 = "Hello HC from %s: %03d\n";
   const char* str2 = "thread";
-  const char* str3 = "Hello again from %s: %d\n";
+  const char* str3 = "Hello again from %s: %03d\n";
 
   parallel_for_each(extent<1>(GLOBAL).tile(TILE), [=](tiled_index<1> tidx) [[hc]] {
       printf(printf_buf, str1, str2, tidx.global[0]);
@@ -33,7 +33,12 @@ int main() {
   return 0;
 }
 
-// CHECK: Hello HC from thread: 0
-// CHECK: Hello again from thread: 0
-// CHECK: Hello HC from thread: 64
-// CHECK: Hello again from thread: 64
+// CHECK-DAG: Hello HC from thread: 000
+// CHECK-DAG: Hello HC from thread: 063
+// CHECK-DAG: Hello HC from thread: 064
+// CHECK-DAG: Hello HC from thread: 127
+
+// CHECK-DAG: Hello again from thread: 000
+// CHECK-DAG: Hello again from thread: 063
+// CHECK-DAG: Hello again from thread: 064
+// CHECK-DAG: Hello again from thread: 127
