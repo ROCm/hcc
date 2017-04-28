7fe38ac18997ec8a491349e033c462af6472b907
Aditya Atluri <adityaatluri@users.noreply.github.com>
2017-04-28 11:51:40 -0500

    Update hc_am.cpp

--- lib/hsa/hc_am.cpp
+++ lib/hsa/hc_am.cpp
@@ -512,7 +512,10 @@ am_status_t am_memory_host_unlock(hc::accelerator &ac, void *hostPtr)
     if(am_status == AM_SUCCESS)
     {
         hsa_status_t hsa_status = hsa_amd_memory_unlock(hostPtr);
-        am_memtracker_remove(hostPtr);
+    }
+    am_status_t remove_status = am_memtracker_remove(hostPtr);
+    if(remove_status != AM_SUCCESS){
+         return remove_status;
     }
     return am_status;
 }
