ebde35406154c28dc6507311fa92a9db8b469f65
Siu Chi Chan <siuchi.chan@amd.com>
2017-08-10 16:38:20 -0400

    add a flag to KalmarContext to indicate whether initialization was successful

--- include/kalmar_runtime.h
+++ include/kalmar_runtime.h
@@ -504,6 +504,9 @@ protected:
     KalmarDevice* def;
     std::vector<KalmarDevice*> Devices;
     KalmarContext() : def(nullptr), Devices() { Devices.push_back(new CPUDevice); }
+
+    bool init_success = false; 
+
 public:
     virtual ~KalmarContext() {}
 
--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -2896,6 +2896,9 @@ public:
 
         hsa_status_t status;
         status = hsa_init();
+        if (status != HSA_STATUS_SUCCESS)
+          return;
+
         STATUS_CHECK(status, __LINE__);
 
         // Iterate over the agents to find out gpu device
@@ -2916,7 +2919,6 @@ public:
             Devices.push_back(Dev);
         }
 
-
 #if SIGNAL_POOL_SIZE > 0
         signalPoolMutex.lock();
 
@@ -2932,6 +2934,7 @@ public:
 
         signalPoolMutex.unlock();
 #endif
+        init_success = true;
     }
 
     void releaseSignal(hsa_signal_t signal, int signalIndex) {
@@ -3057,6 +3060,9 @@ public:
     ~HSAContext() {
         hsa_status_t status = HSA_STATUS_SUCCESS;
         DBOUT(DB_INIT, "HSAContext::~HSAContext() in\n");
+
+        if (!init_success)
+          return;
 
         // destroy all KalmarDevices associated with this context
         for (auto dev : Devices)
