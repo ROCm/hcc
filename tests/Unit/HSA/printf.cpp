5cd6ea6e037e2816af9a2ea85a24eb3f38d12c89
Siu Chi Chan <siuchi.chan@amd.com>
2017-10-02 12:04:16 -0400

    update printf unit test

--- tests/Unit/HSA/printf.cpp
+++ tests/Unit/HSA/printf.cpp
@@ -8,29 +8,23 @@
 #define TILE (64)
 #define GLOBAL (TILE*2)
 
-#define PRINTF_BUFFER_SIZE (16384)
-
 int main() {
   using namespace hc;
 
-  accelerator acc = accelerator();
-  PrintfPacket* printf_buf = createPrintfBuffer(acc, PRINTF_BUFFER_SIZE);
-
-  if (!printf_buf) {
-    std::printf("createPrintfBuffer failed.\n");
-  }
+  hc::array_view<hc::PrintfError,1> err(GLOBAL*2);
 
   parallel_for_each(extent<1>(GLOBAL).tile(TILE), [=](tiled_index<1> tidx) [[hc]] {
       const char* str1 = "Hello HC from %s: %03d\n";
       const char* str2 = "thread";
       const char* str3 = "Hello again from %s: %03d\n";
-      printf(printf_buf, str1, str2, tidx.global[0]);
-      printf(printf_buf, str3, "thread", tidx.global[0]);
+#if 1
+      err[tidx.global[0]*2] = printf(str1, str2, tidx.global[0]);
+      err[tidx.global[0]*2 + 1] = printf(str3, "thread", tidx.global[0]);
+#else
+      printf(str1, str2, tidx.global[0]);
+      printf(str3, "thread", tidx.global[0]);
+#endif
   }).wait();
-
-  processPrintfBuffer(printf_buf);
-
-  deletePrintfBuffer(printf_buf);
 
   return 0;
 }
@@ -41,7 +35,6 @@ int main() {
 // CHECK-DAG: Hello HC from thread: 063
 // CHECK-DAG: Hello HC from thread: 064
 // CHECK-DAG: Hello HC from thread: 127
-
 // CHECK-DAG: Hello again from thread: 000
 // CHECK-DAG: Hello again from thread: 063
 // CHECK-DAG: Hello again from thread: 064
