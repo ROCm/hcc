ed671189d801823f3de13550c076f80a4b3dba54
Siu Chi Chan <siuchi.chan@amd.com>
2017-10-03 16:12:22 -0400

    flush the printf buffer when waiting on a completion future
    Change-Id: I16c8d1544c28676ddce4937144d94b9d6aa33665

--- include/hc.hpp
+++ include/hc.hpp
@@ -1236,6 +1236,8 @@ public:
             //TODO-ASYNC - need to reclaim older AsyncOps here.
             __amp_future.wait();
         }
+
+        Kalmar::getContext()->flushPrintfBuffer();
     }
 
     template <class _Rep, class _Period>
