06e03c40255fc1fabd3df1069a3879b771b43d52
Alex Voicu <alexandru.voicu@amd.com>
2017-11-09 14:20:27 +0000

    This corrects an oversight which resulted in locked globals never being cached.

--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -432,7 +432,6 @@ namespace
                     static mutex mtx;
                     lock_guard<mutex> lck{mtx};
 
-
                     void* p = nullptr;
                     hsa_amd_memory_lock(
                         reinterpret_cast<void*>(it1->second.first),
@@ -444,6 +443,8 @@ namespace
 
                     hsa_executable_agent_global_variable_define(
                         executable, agent, x.c_str(), p);
+
+                    globals.emplace(x, RAII_global{p, hsa_amd_memory_unlock});
                 }
             }
         }
