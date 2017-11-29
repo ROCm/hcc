0b53433c4571e7f272617ecbe5ad099bd2bcd2af
shibata np <shibata-hiroki@ed.tmu.ac.jp>
2017-11-30 06:53:44 +0800

    std::string msg

--- lib/hsa/mcwamp_hsa.cpp
+++ lib/hsa/mcwamp_hsa.cpp
@@ -4657,7 +4657,7 @@ HSADispatch::setLaunchConfiguration(int dims, size_t *globalDims, size_t *localD
 
 	if (max_num_vgprs_per_work_item < workitem_vgpr_count)
 	{
-		msg = "The number of VGPRs (" + std::to_string(kernel->workitem_vgpr_count) + ") needed by this launch (" +
+		std::string msg = "The number of VGPRs (" + std::to_string(kernel->workitem_vgpr_count) + ") needed by this launch (" +
 			(kernel->getKernelName()) + ") exceeds HW (" +
 			max_num_vgprs_per_work_item + ")";
 		throw Kalmar::runtime_exception(msg.c_str(), 0);
