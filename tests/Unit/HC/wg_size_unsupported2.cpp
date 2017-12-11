35cecee3ef9442638718ea51ca35b5f97d8ec500
Siu Chi Chan <siuchi.chan@amd.com>
2017-12-11 11:55:56 -0500

    remove wg_size unit test, create new ones
    Change-Id: Ic8f68ee3a951c08207091d4ea2c0d68ad76ecfe9

---
+++ tests/Unit/HC/wg_size_unsupported2.cpp
@@ -0,0 +1,29 @@
+// RUN: %hc %s -o %t.out && %t.out
+
+#include <hc.hpp>
+#include <string>
+#include <iostream>
+
+int main() {
+  bool pass = false;
+
+  try  {
+    // We expect the runtime will fire an exception due to a large work group size
+    hc::parallel_for_each(hc::extent<2>(8192,1).tile(8192,1), [](hc::tiled_index<2> i) [[hc]] {});
+  } catch (Kalmar::runtime_exception e) {
+    std::string err_str = e.what();
+    pass = err_str.find("The extent of the tile") != std::string::npos &&
+    err_str.find("exceeds the device limit") != std::string::npos;
+  }
+
+  try  {
+    // We expect the runtime will fire an exception due to a large work group size
+    hc::parallel_for_each(hc::extent<2>(1,8192).tile(1,8192), [](hc::tiled_index<2> i) [[hc]] {});
+  } catch (Kalmar::runtime_exception e) {
+    std::string err_str = e.what();
+    pass &= err_str.find("The extent of the tile") != std::string::npos &&
+    err_str.find("exceeds the device limit") != std::string::npos;
+  }
+
+  return pass==true?0:1;
+}
