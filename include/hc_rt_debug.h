b6d287d7620a8729728097ddc097f76e833dfcef
Wen-Heng (Jack) Chung <whchung@gmail.com>
2018-04-09 13:44:12 +0000

    [CMake][Driver][SDK][Runtime] remove libunwind dependency
    It has been verified certain applications with complex shared library loading
    sequence would have exception handlers misplaced due to libunwind. In this
    commit remove libunwind dependency from HCC in different aspect:
    
    - CMake & HCC export CMake scripts
    - hcc-config
    - hc_rt_debug.h
    - Dockerfile
    
    In future commits it can be considered to make this an optional feature which
    is enabled at CMake config time.
    
    Change-Id: I92c9dafffb55c91f204990a4ed437cf3b9d7adb7

--- include/hc_rt_debug.h
+++ include/hc_rt_debug.h
@@ -2,8 +2,6 @@
 
 #include <cstdlib>
 #include <cstdio>
-#define UNW_LOCAL_ONLY
-#include <libunwind.h>
 #ifndef USE_LIBCXX
 #include <cxxabi.h>
 #endif
@@ -78,55 +76,7 @@ extern thread_local ShortTid hcc_tlsShortTid;
 
 namespace hc {
 
-
-  static std::string get_backtrace() {
-    constexpr int buffer_size = 512;
-
-    std::string bt("");
-
-    unw_cursor_t cursor;
-    unw_context_t context;
-    unw_getcontext(&context);
-    unw_init_local(&cursor, &context);
-
-    bt += std::string("Backtrace:\n");
-
-    while(unw_step(&cursor) > 0) {
-      // get the program counter
-      unw_word_t pc;
-      unw_get_reg(&cursor, UNW_REG_IP, &pc);
-      if (pc == 0x00)
-        break;
-
-      // get the function name
-      char func[buffer_size];
-      char* demangled = nullptr;
-      const char* print_func_name;
-      unw_word_t offp;
-      if (unw_get_proc_name(&cursor, func, sizeof(func), &offp) == 0) {
-        int status;
-#ifndef USE_LIBCXX
-        demangled = abi::__cxa_demangle(func, nullptr, nullptr, &status);
-#endif
-        print_func_name = demangled ? demangled : func;
-      }
-      else {
-        print_func_name = "<unknown function>";
-      }
-
-      char loc[buffer_size];
-      std::snprintf(loc, buffer_size, "0x%016lx:\t%s + 0x%lx\n", pc, print_func_name, offp);
-      bt += std::string(loc);
-
-      if (demangled)
-        free(demangled);
-    }
-    return bt;
-  }
-
   static void print_backtrace() {
-    std::string bt = get_backtrace();
-    std::printf("\n%s\n", bt.c_str());
   }
 
 } // namespace hc
