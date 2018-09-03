7529bd96e897a5e215dfdf7a0577bfbd61e54c39
Alex Voicu <alexandru.voicu@amd.com>
2018-09-04 00:23:57 +0100

    Disable floating point atomics for now, since Clang has no support today

--- tests/Unit/HC/hc_atomic_add_float_global.cpp
+++ tests/Unit/HC/hc_atomic_add_float_global.cpp
@@ -14,28 +14,32 @@ using namespace hc;
 #define INIT 0.5f
 
 int main(void) {
-  const int vecSize = 100;
+  #if defined(FLOAT_ATOMICS)
+    const int vecSize = 100;
 
-  // Alloc & init input data
-  std::vector<T> init(vecSize, INIT);
-  array<T, 1> count(vecSize, init.begin());
+    // Alloc & init input data
+    std::vector<T> init(vecSize, INIT);
+    array<T, 1> count(vecSize, init.begin());
 
-  parallel_for_each(count.get_extent(), [=, &count](index<1> idx) [[hc]] {
-    for(unsigned i = 0; i < vecSize; i++) {
-      atomic_fetch_add(&count[i], INIT);
-    }
-  });
-
-  array_view<T, 1> av(count);
-
-  bool ret = true;
-  float sum = std::accumulate(init.begin(), init.end(), 0.0f);
-  sum += INIT;
-  for(unsigned i = 0; i < vecSize; ++i) {
-      if(fabs(av[i] - sum) > TOLERANCE) {
-        ret = false;
+    parallel_for_each(count.get_extent(), [=, &count](index<1> idx) [[hc]] {
+      for(unsigned i = 0; i < vecSize; i++) {
+        atomic_fetch_add(&count[i], INIT);
       }
-  }
+    });
 
-  return !(ret == true);
+    array_view<T, 1> av(count);
+
+    bool ret = true;
+    float sum = std::accumulate(init.begin(), init.end(), 0.0f);
+    sum += INIT;
+    for(unsigned i = 0; i < vecSize; ++i) {
+        if(fabs(av[i] - sum) > TOLERANCE) {
+          ret = false;
+        }
+    }
+
+    return !(ret == true);
+  #else
+    return EXIT_SUCCESS;
+  #endif
 }
--- tests/Unit/HC/hc_atomic_add_float_local.cpp
+++ tests/Unit/HC/hc_atomic_add_float_local.cpp
@@ -12,43 +12,47 @@ using namespace hc;
 #define TOLERANCE 1e-5
 
 int main(void) {
-  const int vecSize = 100;
-  const int tile_size = 10;
+  #if defined(FLOAT_ATOMICS)
+    const int vecSize = 100;
+    const int tile_size = 10;
 
-  // Alloc & init input data
-  extent<2> e_a(vecSize, vecSize);
-  std::vector<T> va(vecSize * vecSize, INIT);
-  array_view<T, 2> av_a(e_a, va); 
+    // Alloc & init input data
+    extent<2> e_a(vecSize, vecSize);
+    std::vector<T> va(vecSize * vecSize, INIT);
+    array_view<T, 2> av_a(e_a, va); 
 
-  extent<2> compute_domain(e_a);
-  parallel_for_each(compute_domain.tile(tile_size, tile_size), [=] (tiled_index<2> tidx) [[hc]] {
-    index<2> localIdx = tidx.local;
-    index<2> globalIdx = tidx.global;
+    extent<2> compute_domain(e_a);
+    parallel_for_each(compute_domain.tile(tile_size, tile_size), [=] (tiled_index<2> tidx) [[hc]] {
+      index<2> localIdx = tidx.local;
+      index<2> globalIdx = tidx.global;
 
-    tile_static T localA[tile_size][tile_size];
-    localA[localIdx[0]][localIdx[1]] = 0;
+      tile_static T localA[tile_size][tile_size];
+      localA[localIdx[0]][localIdx[1]] = 0;
+      tidx.barrier.wait();
+
+      for(int i = 0; i < tile_size; i++) {
+        for(int j = 0; j < tile_size; j++) {
+          atomic_fetch_add(&(localA[i][j]), INIT);
+        }
+      }
     tidx.barrier.wait();
+    av_a[globalIdx[0]][globalIdx[1]] = localA[localIdx[0]][localIdx[1]];
+    });
 
-    for(int i = 0; i < tile_size; i++) {
-      for(int j = 0; j < tile_size; j++) {
-        atomic_fetch_add(&(localA[i][j]), INIT);
+    // accumlate tile_size * tile_size times
+    float sum = 0.0f;
+    for (int i = 0; i < tile_size * tile_size; ++i)
+      sum += INIT;
+    for(unsigned i = 0; i < vecSize; i++) {
+      for(unsigned j = 0; j < vecSize; j++) {
+        if(fabs(av_a(i, j) - sum) > TOLERANCE) {
+          return 1;
+        }
       }
     }
-  tidx.barrier.wait();
-  av_a[globalIdx[0]][globalIdx[1]] = localA[localIdx[0]][localIdx[1]];
-  });
 
-  // accumlate tile_size * tile_size times
-  float sum = 0.0f;
-  for (int i = 0; i < tile_size * tile_size; ++i)
-    sum += INIT;
-  for(unsigned i = 0; i < vecSize; i++) {
-    for(unsigned j = 0; j < vecSize; j++) {
-      if(fabs(av_a(i, j) - sum) > TOLERANCE) {
-        return 1;
-      }
-    }
-  }
-
-  return 0;
+    return 0;
+  #else
+    return EXIT_SUCCESS;
+  #endif
 }
