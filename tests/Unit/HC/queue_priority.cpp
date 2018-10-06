2eb40de2254050b6160ee7301e58a6db8ac4037f
Maneesh Gupta <maneesh.gupta@amd.com>
2018-10-06 10:11:32 +0530

    Make queue_priority unit test to be functional as well

--- tests/Unit/HC/queue_priority.cpp
+++ tests/Unit/HC/queue_priority.cpp
@@ -2,6 +2,9 @@
 // RUN: %hc %s -o %t.out && %t.out
 
 #include <hc.hpp>
+#include <iostream>
+
+#define DATA_LEN 7
 
 // test new enumeration in hc::accelerator_view : queue_priority
 int main() {
@@ -20,15 +23,46 @@ int main() {
   // create one accelerator_view with priority high
   accelerator_view av_high_priority = acc.create_view(execute_in_order, queuing_mode_automatic, priority_high);
 
+  hc::extent<1> e(DATA_LEN);
+
   // test dispatch a kernel to av
-  parallel_for_each(av, extent<1>(1), []() [[hc]] {});
+  hc::array_view<unsigned int, 1> data1(DATA_LEN);
+  parallel_for_each(av, e, [=](hc::index<1> i) [[hc]] {
+    unsigned int flat_id = i[0];
+    data1[flat_id] = flat_id;
+  });
 
   // test dispatch a kernel to av_normal_priority
-  parallel_for_each(av_normal_priority, extent<1>(1), []() [[hc]] {});
+  hc::array_view<unsigned int, 1> data2(DATA_LEN);
+  parallel_for_each(av_normal_priority, e, [=](hc::index<1> i) [[hc]] {
+    unsigned int flat_id = i[0];
+    data2[flat_id] = flat_id;
+  });
 
   // test dispatch a kernel to av_high_priority
-  parallel_for_each(av_high_priority, extent<1>(1), []() [[hc]] {});
+  hc::array_view<unsigned int, 1> data3(DATA_LEN);
+  parallel_for_each(av_high_priority, e, [=](hc::index<1> i) [[hc]] {
+    unsigned int flat_id = i[0];
+    data3[flat_id] = flat_id;
+  });
 
-  return 0;
+  // verify data
+  int errors = 0;
+  for (int i = 0; i < DATA_LEN; ++i) {
+    if (data1[i] != i) {
+      ++errors;
+      std::cerr << "Error data1[" << i << "], expected " << i << ", actual " << data1[i] << std::endl;
+    }
+    if (data2[i] != i) {
+      ++errors;
+      std::cerr << "Error data2[" << i << "], expected " << i << ", actual " << data2[i] << std::endl;
+    }
+    if (data3[i] != i) {
+      ++errors;
+      std::cerr << "Error data3[" << i << "], expected " << i << ", actual " << data3[i] << std::endl;
+    }
+  }
+
+  return errors==0?0:1;
 }
 
