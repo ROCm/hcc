c5d5b5c57f80b11de6c2517e339d75100612602e
eshcherb <33529668+eshcherb@users.noreply.github.com>
2019-01-15 18:13:32 -0600

    Roctracer hip frontend 181113 (#937)
    * activity tracing API
    
    * get rid of current.device_id/queue_id and GetActivityCoord()
    
    * FATAL_ERROR if Profiling API header not found
    
    * adding mutex to callbacks table to resolve multiple threads modification possibility
    
    * adding activity id callback
    
    * minor fixes / cleanup
    
    * cleanup
    
    * activity callbacks table restructuring
    
    * compilatino fix
    
    * style fixes
    
    * removed ACtivityProf template parameter
    
    * compilation fix with disabled roctracer
    
    * removing _ANY op id
    
    * lock free callback getter
    
    * cosmetic change
    
    * adding common activity callback
    
    * compilation fix with disabled roctracer
    
    * set_enabled return false if tracer disabled
    
    * removing HCC_PROFILE dependency

--- lib/mcwamp_impl.hpp
+++ lib/mcwamp_impl.hpp
@@ -5,3 +5,8 @@
 typedef void* (*PushArgImpl_t)(void *, int, size_t, const void *);
 typedef void* (*PushArgPtrImpl_t)(void *, int, size_t, const void *);
 typedef void* (*GetContextImpl_t)();
+
+// Activity profiling routines
+typedef void (*InitActivityCallbackImpl_t)(void*, void*, void*);
+typedef bool (*EnableActivityCallbackImpl_t)(unsigned, bool);
+typedef const char* (*GetCmdNameImpl_t)(unsigned);
